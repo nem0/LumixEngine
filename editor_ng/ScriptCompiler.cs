662414a4d2f302f2e4ef57acc5229f61c8385598
Mikulas Florek <mikulas.florek@gamedev.sk>
2013-11-30 17:12:20 +0100

    the script compiler attached to the asset list

--- editor_ng/ScriptCompiler.cs
+++ editor_ng/ScriptCompiler.cs
@@ -14,39 +14,78 @@ namespace editor_ng
         {
             public Process process;
             public string script_name;
+            public event EventHandler onCompiled;
+
+            public void compiled()
+            {
+                if (onCompiled != null)
+                {
+                    onCompiled(this, null);
+                }
+            }
         };
 
-        Notifications m_notifications;
-        List<CompilationProcess> m_processes = new List<CompilationProcess>();
+        private List<CompilationProcess> m_processes = new List<CompilationProcess>();
 
-
-        public event EventHandler onAllScriptsCompiled;
-
-        public ScriptCompiler(Notifications notifications)
+        public class CompileErrorEventArgs : EventArgs
         {
-            m_notifications = notifications;
+            public string error_message;
+            public string script_name;
+        };
+
+        public class ScriptUpdatedEventArgs : EventArgs
+        {
+            public string script_name;
+        };
+
+        public event EventHandler onCompileError;
+        public event EventHandler onAllScriptsCompiled;
+        public event EventHandler onScriptUpdated;
+
+        void process_onCompiled(object sender, EventArgs e)
+        {
+            if (onScriptUpdated != null)
+            {
+                CompilationProcess process = sender as CompilationProcess;
+                ScriptUpdatedEventArgs args = new ScriptUpdatedEventArgs();
+                args.script_name = process.script_name;
+                onScriptUpdated(this, args);
+            }
         }
 
+        public void compile(string path, string filename, bool emit_event)
+        {
+            CompilationProcess p = new CompilationProcess();
+            if (emit_event)
+            {
+                p.onCompiled += process_onCompiled;
+            }
+            compile(p, path, filename);
+        }
+
+        private void compile(CompilationProcess process, string path, string filename)
+        {
+            process.script_name = filename;
+            process.process = new Process();
+            ProcessStartInfo start_info = new ProcessStartInfo();
+            process.process.StartInfo.FileName = "cmd.exe";
+            process.process.StartInfo.Arguments = "/C " + System.IO.Directory.GetCurrentDirectory() + "\\scripts\\compile.bat " + path;
+            process.process.StartInfo.CreateNoWindow = true;
+            process.process.StartInfo.UseShellExecute = false;
+            process.process.StartInfo.RedirectStandardOutput = true;
+            process.process.StartInfo.Verb = "execute";
+            process.process.Exited += process_Exited;
+            process.process.EnableRaisingEvents = true;
+            m_processes.Add(process);
+            process.process.Start();
+        }
 
         public void compileAllScripts()
         {
             DirectoryInfo dir_info = new DirectoryInfo("scripts");
             foreach (FileInfo file in dir_info.GetFiles("*.cpp"))
             {
-                CompilationProcess process = new CompilationProcess();
-                process.script_name = file.Name;
-                process.process = new Process();
-                ProcessStartInfo start_info = new ProcessStartInfo();
-                process.process.StartInfo.FileName = "cmd.exe";
-                process.process.StartInfo.Arguments = "/C " + System.IO.Directory.GetCurrentDirectory() + "\\scripts\\compile.bat " + file.FullName;
-                process.process.StartInfo.CreateNoWindow = true;
-                process.process.StartInfo.UseShellExecute = false;
-                process.process.StartInfo.RedirectStandardOutput = true;
-                process.process.StartInfo.Verb = "execute";
-                process.process.Exited += process_Exited;
-                process.process.EnableRaisingEvents = true;
-                m_processes.Add(process);
-                process.process.Start();
+                compile(new CompilationProcess(), file.FullName, file.Name);   
             }
         }
 
@@ -61,11 +100,13 @@ namespace editor_ng
             {
                 if (p.process == sender)
                 {
-                    if (p.process.ExitCode != 0)
+                    p.compiled();
+                    if (p.process.ExitCode != 0 && onCompileError != null)
                     {
-                        //string output = process.StandardOutput.ReadToEnd();
-                        //error notification does not work here
-                        m_notifications.invokeNotification("Script " + p.script_name + " failed to compile");
+                        CompileErrorEventArgs args = new CompileErrorEventArgs();
+                        args.error_message = p.process.StandardOutput.ReadToEnd();
+                        args.script_name = p.script_name;
+                        onCompileError(this, args);
                     }
                     m_processes.Remove(p);
                     if (m_processes.Count == 0 && onAllScriptsCompiled != null)
