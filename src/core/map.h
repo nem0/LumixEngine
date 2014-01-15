08b5803c332ccdbc5c05459d71a61ed59c9799e7
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-01-15 20:01:08 +0100

    fixed crash in map

--- src/core/map.h
+++ src/core/map.h
@@ -263,7 +263,7 @@ class map
 				node->right = insert(key, node->right, node, new_node);
 				if(node->getRightHeight() - node->getLeftHeight() == 2)
 				{
-					if(key < node->right->key)
+					if(node->right->key < key)
 					{
 						ret = rotateLeft(node);
 					}
