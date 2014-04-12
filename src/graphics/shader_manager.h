af984e201d8fac06650920e015605b65c80c72c3
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-04-13 00:27:11 +0200

    shaders use shared buffer

--- src/graphics/shader_manager.h
+++ src/graphics/shader_manager.h
@@ -7,11 +7,17 @@ namespace Lux
 	class LUX_ENGINE_API ShaderManager : public ResourceManagerBase
 	{
 	public:
-		ShaderManager() : ResourceManagerBase() {}
-		~ShaderManager() {}
+		ShaderManager();
+		~ShaderManager();
+
+		char* getBuffer(int32_t size);
 
 	protected:
 		virtual Resource* createResource(const Path& path) override;
 		virtual void destroyResource(Resource& resource) override;
+
+	private:
+		uint8_t* m_buffer;
+		int32_t m_buffer_size;
 	};
 }
