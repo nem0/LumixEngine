fe7b09340a8ee684b35ac507d03d79164d97893b
tluqo <tluqo@azet.sk>
2014-04-13 19:29:53 +0200

    - cleaning

--- src/core/file_events_device.cpp
+++ src/core/file_events_device.cpp
@@ -19,147 +19,101 @@ namespace Lux
 			{
 			}
 
+
 			virtual ~EventsFile() 
 			{
 			}
 
+
 			virtual bool open(const char* path, Mode mode) override
 			{
-				Event event;
-				event.type = EventType::OPEN_BEGIN;
-				event.handle = uintptr_t(this);
-				event.path = path;
-				event.ret = -1;
-				event.param = -1;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::OPEN_BEGIN, path, -1, mode);
 				bool ret = m_file.open(path, mode);
+				invokeEvent(EventType::OPEN_FINISHED, path, ret ? 1 : 0, mode);
 
-				event.type = EventType::OPEN_FINISHED;
-				event.ret = ret ? 1 : 0;
-				event.param = -1;
-				m_cb.invoke(event);
 				return ret;
 			}
+
 
 			virtual void close() override
 			{
-				Event event;
-				event.type = EventType::CLOSE_BEGIN;
-				event.handle = uintptr_t(this);
-				event.path = "";
-				event.ret = -1;
-				event.param = -1;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::CLOSE_BEGIN, "", -1, -1);
 				m_file.close();
 
-				event.type = EventType::CLOSE_FINISHED;
-				m_cb.invoke(event);
+				invokeEvent(EventType::CLOSE_FINISHED, "", -1, -1);
 			}
+
 
 			virtual bool read(void* buffer, size_t size) override
 			{
-				Event event;
-				event.type = EventType::READ_BEGIN;
-				event.handle = uintptr_t(this);
-				event.path = "";
-				event.ret = -1;
-				event.param = size;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::READ_BEGIN, "", -1, size);
 				bool ret = m_file.read(buffer, size);
 
-				event.type = EventType::READ_FINISHED;
-				event.ret = ret ? 1 : 0;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::READ_FINISHED, "", ret ? 1 : 0, size);
 				return ret;
 			}
+
 
 			virtual bool write(const void* buffer, size_t size) override
 			{
-				Event event;
-				event.type = EventType::WRITE_BEGIN;
-				event.handle = uintptr_t(this);
-				event.path = "";
-				event.ret = -1;
-				event.param = size;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::WRITE_BEGIN, "", -1, size);
 				bool ret = m_file.write(buffer, size);
 
-				event.type = EventType::WRITE_FINISHED;
-				event.ret = ret ? 1 : 0;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::WRITE_FINISHED, "", ret ? 1 : 0, size);
 				return ret;
 			}
+
 
 			virtual const void* getBuffer() const override
 			{
 				return NULL;
 			}
 
+
 			virtual size_t size() override
 			{
-				Event event;
-				event.type = EventType::SIZE_BEGIN;
-				event.handle = uintptr_t(this);
-				event.path = "";
-				event.ret = -1;
-				event.param = -1;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::SIZE_BEGIN, "", -1, -1);
 				size_t ret = m_file.size();
 
-				event.type = EventType::SIZE_FINISHED;
-				event.ret = ret;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::SIZE_FINISHED, "", ret, -1);
 				return ret;
 			}
+
 
 			virtual size_t seek(SeekMode base, size_t pos) override
 			{
-				Event event;
-				event.type = EventType::SEEK_BEGIN;
-				event.handle = uintptr_t(this);
-				event.path = "";
-				event.ret = pos;
-				event.param = base;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::SEEK_BEGIN, "", pos, base);
 				size_t ret = m_file.seek(base, pos);
 
-
-				event.type = EventType::SEEK_FINISHED;
-				event.ret = ret;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::SEEK_FINISHED, "", ret, base);
 				return ret;
 			}
+
 
 			virtual size_t pos() override
 			{
-				Event event;
-				event.type = EventType::POS_BEGIN;
-				event.handle = uintptr_t(this);
-				event.path = "";
-				event.ret = -1;
-				event.param = -1;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::POS_BEGIN, "", -1, -1);
 				size_t ret = m_file.pos();
 
-				event.type = EventType::POS_FINISHED;
-				event.ret = ret;
-
-				m_cb.invoke(event);
+				invokeEvent(EventType::POS_FINISHED, "", ret, -1);
 				return ret;
 			}
 
+
 		private:
+			void invokeEvent(EventType type, const char* path, int32_t ret, int32_t param)
+			{
+				Event event;
+				event.type = type;
+				event.handle = uintptr_t(this);
+				event.path = path;
+				event.ret = ret;
+				event.param = param;
+
+				m_cb.invoke(event);
+			}
+
+
 			IFile& m_file;
 			EventCallback& m_cb;
 		};
