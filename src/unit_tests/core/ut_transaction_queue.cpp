1268343f981f62243e2dfeebe3ebc66a39f7305e
tluqo <tluqo@azet.sk>
2014-05-04 20:13:17 +0200

    - fixed issues in the transaction queue

--- src/unit_tests/core/ut_transaction_queue.cpp
+++ src/unit_tests/core/ut_transaction_queue.cpp
@@ -18,12 +18,12 @@ namespace
 	class TestTaskConsumer : public Lux::MT::Task
 	{
 	public:
-		TestTaskConsumer(TransQueue* queue, Test* array) 
-			: m_trans_queue(queue) 
+		TestTaskConsumer(TransQueue* queue, Test* array)
+			: m_trans_queue(queue)
 			, m_array(array)
 		{}
 
-		~TestTaskConsumer() 
+		~TestTaskConsumer()
 		{}
 
 		int task()
@@ -52,13 +52,13 @@ namespace
 	class TestTaskProducer : public Lux::MT::Task
 	{
 	public:
-		TestTaskProducer(TransQueue* queue, Test* array, size_t size) 
-			: m_trans_queue(queue) 
+		TestTaskProducer(TransQueue* queue, Test* array, size_t size)
+			: m_trans_queue(queue)
 			, m_array(array)
 			, m_size(size)
 		{}
 
-		~TestTaskProducer() 
+		~TestTaskProducer()
 		{}
 
 		int task()
@@ -79,10 +79,8 @@ namespace
 		Test* m_array;
 		size_t m_size;
 	};
-	
-	TODO("unit test empty!");
 
-	void UT_transaction_queue(const char* params)
+	void UT_tq_heavy_usage(const char* params)
 	{
 		const size_t itemsCount = 1200000;
 		Test* testItems = LUX_NEW_ARRAY(Test, itemsCount);
@@ -93,73 +91,62 @@ namespace
 			testItems[i].thread_id = Lux::MT::getCurrentThreadID();
 		}
 
-		TransQueue transQueue;
+		TransQueue trans_queue;
 
-		TestTaskConsumer* cons1 = LUX_NEW(TestTaskConsumer)(&transQueue, testItems);
-		TestTaskConsumer* cons2 = LUX_NEW(TestTaskConsumer)(&transQueue, testItems);
-		TestTaskConsumer* cons3 = LUX_NEW(TestTaskConsumer)(&transQueue, testItems);
-		TestTaskConsumer* cons4 = LUX_NEW(TestTaskConsumer)(&transQueue, testItems);
+		TestTaskConsumer cons1(&trans_queue, testItems);
+		TestTaskConsumer cons2(&trans_queue, testItems);
+		TestTaskConsumer cons3(&trans_queue, testItems);
+		TestTaskConsumer cons4(&trans_queue, testItems);
 
-		cons1->create("cons1");
-		cons2->create("cons2");
-		cons3->create("cons3");
-		cons4->create("cons4");
+		cons1.create("cons1");
+		cons2.create("cons2");
+		cons3.create("cons3");
+		cons4.create("cons4");
 
-		cons1->run();
-		cons2->run();
-		cons3->run();
-		cons4->run();
+		cons1.run();
+		cons2.run();
+		cons3.run();
+		cons4.run();
 
-		TestTaskProducer* prod1 = LUX_NEW(TestTaskProducer)(&transQueue, &testItems[0], itemsCount / 4);
-		TestTaskProducer* prod2 = LUX_NEW(TestTaskProducer)(&transQueue, &testItems[itemsCount / 4], itemsCount / 4);
-		TestTaskProducer* prod3 = LUX_NEW(TestTaskProducer)(&transQueue, &testItems[itemsCount / 2], itemsCount / 4);
-		TestTaskProducer* prod4 = LUX_NEW(TestTaskProducer)(&transQueue, &testItems[3 * itemsCount / 4], itemsCount / 4);
+		TestTaskProducer prod1(&trans_queue, &testItems[0], itemsCount / 4);
+		TestTaskProducer prod2(&trans_queue, &testItems[itemsCount / 4], itemsCount / 4);
+		TestTaskProducer prod3(&trans_queue, &testItems[itemsCount / 2], itemsCount / 4);
+		TestTaskProducer prod4(&trans_queue, &testItems[3 * itemsCount / 4], itemsCount / 4);
 
-		prod1->create("prod1");
-		prod2->create("prod2");
-		prod3->create("prod3");
-		prod4->create("prod4");
+		prod1.create("prod1");
+		prod2.create("prod2");
+		prod3.create("prod3");
+		prod4.create("prod4");
 
-		prod1->run();
-		prod2->run();
-		prod3->run();
-		prod4->run();
+		prod1.run();
+		prod2.run();
+		prod3.run();
+		prod4.run();
 
-		while (!prod1->isFinished() 
-			|| !prod2->isFinished()
-			|| !prod3->isFinished()
-			|| !prod4->isFinished())
+		while (!prod1.isFinished()
+			|| !prod2.isFinished()
+			|| !prod3.isFinished()
+			|| !prod4.isFinished()
+			|| !trans_queue.isEmpty())
 			Lux::MT::yield();
 
-		while (!transQueue.isEmpty())
-			Lux::MT::yield();
+		trans_queue.abort();
+		trans_queue.abort();
+		trans_queue.abort();
+		trans_queue.abort();
 
-		prod1->destroy();
-		prod2->destroy();
-		prod3->destroy();
-		prod4->destroy();
+		prod1.destroy();
+		prod2.destroy();
+		prod3.destroy();
+		prod4.destroy();
 
-		transQueue.abort();
-		transQueue.abort();
-		transQueue.abort();
-		transQueue.abort();
+		cons1.destroy();
+		cons2.destroy();
+		cons3.destroy();
+		cons4.destroy();
 
-		cons1->destroy();
-		cons2->destroy();
-		cons3->destroy();
-		cons4->destroy();
-
-		LUX_DELETE(cons1);
-		LUX_DELETE(cons2);
-		LUX_DELETE(cons3);
-		LUX_DELETE(cons4);
-
-		LUX_DELETE(prod1);
-		LUX_DELETE(prod2);
-		LUX_DELETE(prod3);
-		LUX_DELETE(prod4);
-
-		Lux::g_log_info.log("Unit", "UT_transaction_queue finishing ...");
+		Lux::g_log_info.log("Unit", "UT_tq_heavy_usage is finishing ...");
+		Lux::g_log_info.log("Unit", "UT_tq_heavy_usage is checking results ...");
 
 		for (size_t i = 0; i < itemsCount; i++)
 		{
@@ -170,8 +157,55 @@ namespace
 
 		LUX_DELETE_ARRAY(testItems);
 
-		Lux::g_log_info.log("Unit", "UT_transaction_queue finished ...");
+		Lux::g_log_info.log("Unit", "UT_tq_heavy_usage finished ...");
 	};
+
+	void UT_tq_push(const char* params)
+	{
+		const size_t itemsCount = 1200000;
+		Test* testItems = LUX_NEW_ARRAY(Test, itemsCount);
+		for (size_t i = 0; i < itemsCount; i++)
+		{
+			testItems[i].idx = i;
+			testItems[i].proc_count = 0;
+			testItems[i].thread_id = Lux::MT::getCurrentThreadID();
+		}
+
+		TransQueue trans_queue;
+
+		TestTaskProducer prod(&trans_queue, &testItems[0], itemsCount);
+		TestTaskConsumer cons(&trans_queue, testItems);
+
+		prod.create("producer");
+		cons.create("consumer");
+
+		prod.run();
+		Lux::MT::sleep(1000);
+		cons.run();
+
+		while (!prod.isFinished() || !trans_queue.isEmpty())
+			Lux::MT::yield();
+
+		trans_queue.abort();
+
+		prod.destroy();
+		cons.destroy();
+
+		Lux::g_log_info.log("Unit", "UT_tq_push is finishing ...");
+		Lux::g_log_info.log("Unit", "UT_tq_push is checking results ...");
+
+		for (size_t i = 0; i < itemsCount; i++)
+		{
+			LUX_EXPECT_EQ(testItems[i].idx, i);
+			LUX_EXPECT_EQ(testItems[i].proc_count, 1);
+			LUX_EXPECT_NE(testItems[i].thread_id, Lux::MT::getCurrentThreadID());
+		}
+
+		LUX_DELETE_ARRAY(testItems);
+
+		Lux::g_log_info.log("Unit", "UT_tq_heavy_usage finished ...");
+	}
 }
 
-REGISTER_TEST("unit_tests/core/multi_thread/transaction_queue", UT_transaction_queue, "")
+REGISTER_TEST("unit_tests/core/multi_thread/transaction_queue_heavy_usage", UT_tq_heavy_usage, "");
+REGISTER_TEST("unit_tests/core/multi_thread/transaction_queue_push", UT_tq_push, "");
