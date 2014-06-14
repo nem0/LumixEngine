e9ab54a38cb5b652b6d670513f9b61b01101e6d7
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-06-14 15:06:41 +0200

    profiler does not mix frames

--- qteditor/QtEditor/profilergraph.cpp
+++ qteditor/QtEditor/profilergraph.cpp
@@ -12,6 +12,7 @@ ProfilerGraph::ProfilerGraph(QWidget *parent)
 	m_ui->setupUi(this);
 	m_frame = 0;
 	m_block = NULL;
+	m_model = NULL;
 }
 
 ProfilerGraph::~ProfilerGraph()
@@ -22,7 +23,7 @@ ProfilerGraph::~ProfilerGraph()
 
 void ProfilerGraph::mousePressEvent(QMouseEvent* event)
 {
-	m_frame = Lux::Profiler::Block::MAX_FRAMES * event->x() / width();
+	m_frame = m_model->getRoot() ? m_model->getRoot()->m_frames.size() * event->x() / width() : 0;
 	update();
 	emit frameSet();
 }
@@ -32,25 +33,26 @@ void ProfilerGraph::mouseMoveEvent(QMouseEvent* event)
 {
 	if(event->buttons() & Qt::LeftButton)
 	{
-		m_frame = Lux::Profiler::Block::MAX_FRAMES * event->x() / width();
+		m_frame = m_model->getRoot() ? m_model->getRoot()->m_frames.size() * event->x() / width() : 0;
 		update();
 		emit frameSet();
 	}
 }
 
 
-void ProfilerGraph::getBlockPath(Lux::Profiler::Block* block, QPainterPath& path, float max)
+void ProfilerGraph::getBlockPath(ProfileModel::Block* block, QPainterPath& path, float max)
 {
 	int w = width();
 	int h = height();
-	const Lux::Profiler::Block::Frame* frame = block->m_frames;
 
 	path.moveTo(0, height());
-	for(int i = 0; i < Lux::Profiler::Block::MAX_FRAMES - 1; ++i)
+	
+	int i = m_model->getRoot()->m_frames.size() - block->m_frames.size();
+	for(auto iter = block->m_frames.begin(), end = block->m_frames.end(); iter != end; ++iter)
 	{
-		int idx = (block->m_frame_index + 1 + i) % Lux::Profiler::Block::MAX_FRAMES;
-		float l = i * w / (float)Lux::Profiler::Block::MAX_FRAMES;
-		float t = (h - 1) * (1.0f - frame[idx].m_length / max);
+		float l = i * w / (float)m_model->getRoot()->m_frames.size();
+		++i;
+		float t = (h - 1) * (1.0f - *iter / max);
 		path.lineTo(l, t);
 	}
 	path.lineTo(width(), height());
@@ -63,7 +65,7 @@ void ProfilerGraph::paintEvent(QPaintEvent*)
 {
 	QPainter painter(this);
 	painter.setRenderHint(QPainter::RenderHint::Antialiasing);
-	Lux::Profiler::Block* root = Lux::g_profiler.getRootBlock();
+	ProfileModel::Block* root = m_model->getRoot();
 	if(!root)
 	{
 		return;
@@ -74,11 +76,10 @@ void ProfilerGraph::paintEvent(QPaintEvent*)
 	gradient.setColorAt(1.0, QColor(0, 64, 0, 128));
 	gradient.setSpread(QGradient::Spread::ReflectSpread);
 
-	const Lux::Profiler::Block::Frame* frame = root->m_frames;
 	float max = 0;
-	for(int i = 0; i < Lux::Profiler::Block::MAX_FRAMES; ++i)
+	for(auto iter = root->m_frames.begin(), end = root->m_frames.end(); iter != end; ++iter)
 	{
-		max = max < frame[i].m_length ? frame[i].m_length : max;
+		max = max < *iter ? *iter : max;
 	}
 
 	QPainterPath path;
@@ -86,11 +87,6 @@ void ProfilerGraph::paintEvent(QPaintEvent*)
 	painter.fillPath(path, gradient);
 	if(m_block && m_block != root)
 	{
-		/*ASSERT(m_block->m_frames[0].m_index == root->m_frames[0].m_index);
-		for(int i = 0; i < Lux::Profiler::Block::MAX_FRAMES; ++i)
-		{
-			ASSERT(m_block->m_frames[i].m_length < root->m_frames[i].m_length);
-		}*/
 		QPainterPath detail_path;
 		getBlockPath(m_block, detail_path, max);
 		gradient.setColorAt(0.0, QColor(0, 0, 255, 192));
@@ -99,6 +95,6 @@ void ProfilerGraph::paintEvent(QPaintEvent*)
 	}
 
 	painter.setPen(QColor(255, 0, 0));
-	float l = m_frame * width() / (float)Lux::Profiler::Block::MAX_FRAMES;
+	float l = m_frame * width() / m_model->getRoot()->m_frames.size();
 	painter.drawLine(QPointF(l, 0), QPointF(l, height()));
 }
