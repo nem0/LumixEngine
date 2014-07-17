bab824b635c9999576a901603c53edade1f980a4
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-07-17 21:23:23 +0200

    physics scene refactor

--- src/physics/physics_scene.h
+++ src/physics/physics_scene.h
@@ -26,40 +26,36 @@ class LUMIX_PHYSICS_API PhysicsScene
 {
 	friend class PhysicsSystem;
 	public:
-		PhysicsScene();
+		static PhysicsScene* create(PhysicsSystem& system, Universe& universe, Engine& engine);
+		static void destroy(PhysicsScene* scene);
 		
-		bool create(PhysicsSystem& system, Universe& universe, Engine& engine);
-		void destroy();
-		void update(float time_delta);
-		void render();
-		bool raycast(const Vec3& origin, const Vec3& dir, float distance, RaycastHit& result);
-		Component createBoxRigidActor(Entity entity);
-		Component createMeshRigidActor(Entity entity);
-		Component createController(Entity entity);
-		Component createHeightfield(Entity entity);
-		void destroyActor(Component cmp);
-		PhysicsSystem& getSystem() const;
+		virtual ~PhysicsScene() {}
+		virtual void update(float time_delta) = 0;
+		virtual void render() = 0;
+		virtual bool raycast(const Vec3& origin, const Vec3& dir, float distance, RaycastHit& result) = 0;
+		virtual Component createBoxRigidActor(Entity entity) = 0;
+		virtual Component createMeshRigidActor(Entity entity) = 0;
+		virtual Component createController(Entity entity) = 0;
+		virtual Component createHeightfield(Entity entity) = 0;
+		virtual PhysicsSystem& getSystem() const = 0;
 		
-		void getIsDynamic(Component cmp, bool& is);
-		void setIsDynamic(Component cmp, const bool& is);
-		void getHalfExtents(Component cmp, Vec3& size);
-		void setHalfExtents(Component cmp, const Vec3& size);
-		void getShapeSource(Component cmp, string& str);
-		void setShapeSource(Component cmp, const string& str);
-		void getHeightmap(Component cmp, string& str);
-		void setHeightmap(Component cmp, const string& str);
-		void getHeightmapXZScale(Component cmp, float& scale);
-		void setHeightmapXZScale(Component cmp, const float& scale);
-		void getHeightmapYScale(Component cmp, float& scale);
-		void setHeightmapYScale(Component cmp, const float& scale);
+		virtual void getIsDynamic(Component cmp, bool& is) = 0;
+		virtual void setIsDynamic(Component cmp, const bool& is) = 0;
+		virtual void getHalfExtents(Component cmp, Vec3& size) = 0;
+		virtual void setHalfExtents(Component cmp, const Vec3& size) = 0;
+		virtual void getShapeSource(Component cmp, string& str) = 0;
+		virtual void setShapeSource(Component cmp, const string& str) = 0;
+		virtual void getHeightmap(Component cmp, string& str) = 0;
+		virtual void setHeightmap(Component cmp, const string& str) = 0;
+		virtual void getHeightmapXZScale(Component cmp, float& scale) = 0;
+		virtual void setHeightmapXZScale(Component cmp, const float& scale) = 0;
+		virtual void getHeightmapYScale(Component cmp, float& scale) = 0;
+		virtual void setHeightmapYScale(Component cmp, const float& scale) = 0;
 
-		void moveController(Component cmp, const Vec3& v, float dt);
+		virtual void moveController(Component cmp, const Vec3& v, float dt) = 0;
 	
-		void serialize(ISerializer& serializer);
-		void deserialize(ISerializer& serializer);
-
-	private:
-		struct PhysicsSceneImpl* m_impl;
+		virtual void serialize(ISerializer& serializer) = 0;
+		virtual void deserialize(ISerializer& serializer) = 0;
 };
 
 
