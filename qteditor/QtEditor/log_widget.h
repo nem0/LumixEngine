11acbfc710268c66b67f72487bb43155d0cf70b3
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-07-18 21:50:28 +0200

    update according to review

--- qteditor/QtEditor/log_widget.h
+++ qteditor/QtEditor/log_widget.h
@@ -1,62 +1,31 @@
 #pragma once
 
-
-
 #include <QDockWidget>
 
-
-
 namespace Ui
-
 {
-
 	class LogWidget;
-
 }
 
-
-
 class LogWidget : public QDockWidget
-
 {
-
 	Q_OBJECT
 
-
-
 public:
-
 	explicit LogWidget(QWidget* parent = NULL);
-
 	~LogWidget();
 
-
-
 signals:
-
 	void infoReceived(const QString& system, const QString& message);
-
-
-
+	
 private slots:
-
 	void on_clearButton_clicked();
-
 	void onInfoReceived(const QString& system, const QString& message);
 
-
-
 private:
-
 	void onInfo(const char* system, const char* message);
 
-
-
 private:
-
 	Ui::LogWidget* m_ui;
-
 };
-
-
 
