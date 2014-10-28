e4ca1d5c6608293332a019b62988406a2ec56574
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-10-28 22:47:28 +0100

    copy/paste entities - closes #265

--- src/editor/world_editor.h
+++ src/editor/world_editor.h
@@ -70,6 +70,8 @@ namespace Lumix
 			virtual void saveUniverse(const Path& path) = 0;
 			virtual void newUniverse() = 0;
 			virtual Path getUniversePath() const = 0;
+			virtual void copyEntity() = 0;
+			virtual void pasteEntity() = 0;
 			virtual void addComponent(uint32_t type_crc) = 0;
 			virtual void cloneComponent(const Component& src, Entity& entity) = 0;
 			virtual void destroyComponent(const Component& crc) = 0;
