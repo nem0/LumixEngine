827f6e0eab4d40c24684a10be6dd9c53bc255567
tluqo <tluqo@azet.sk>
2014-11-13 23:06:42 +0100

    hash map fix.

--- src/unit_tests/core/ut_hash_map.cpp
+++ src/unit_tests/core/ut_hash_map.cpp
@@ -1,5 +1,6 @@
 #include "unit_tests/suite/lumix_unit_tests.h"
 
+#include "core/array.h"
 #include "core/hash_map.h"
 
 namespace
@@ -11,6 +12,15 @@ namespace
 
 		LUMIX_EXPECT_TRUE(hash_table.empty());
 	};
+
+	void UT_array(const char* params)
+	{
+		Lumix::DefaultAllocator allocator;
+		Lumix::HashMap<int32_t, Lumix::Array<int> > hash_table(allocator);
+
+		LUMIX_EXPECT_TRUE(hash_table.empty());
+	};
 }
 
 REGISTER_TEST("unit_tests/core/hash_map/insert", UT_insert, "")
+REGISTER_TEST("unit_tests/core/hash_map/array", UT_array, "")
