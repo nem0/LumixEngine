b0ee8f1e6808b33f7cff09e3415b53475626ff75
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-11-13 19:37:53 +0100

    sentinel in the debug allocator to save a few branches

--- src/debug/allocator.h
+++ src/debug/allocator.h
@@ -14,8 +14,21 @@ namespace Debug
 {
 
 
+	class StackNode;
+
+
 	class LUMIX_CORE_API Allocator : public IAllocator
 	{
+		private:
+			class AllocationInfo
+			{
+			public:
+				AllocationInfo* m_previous;
+				AllocationInfo* m_next;
+				size_t m_size;
+				StackNode* m_stack_leaf;
+			};
+
 		public:
 			Allocator(IAllocator& source);
 			virtual ~Allocator();
@@ -30,6 +43,7 @@ namespace Debug
 			class StackTree* m_stack_tree;
 			MT::SpinMutex m_mutex;
 			class AllocationInfo* m_root;
+			AllocationInfo m_sentinels[2];
 	};
 
 
