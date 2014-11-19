337efc82763eca3c75cae07430a93bb1e2846ac1
tluqo <tluqo@azet.sk>
2014-11-19 23:23:50 +0100

    fixed hashMap clear -> added rehash (fixes #344)

--- src/unit_tests/core/ut_hash_map.cpp
+++ src/unit_tests/core/ut_hash_map.cpp
@@ -20,7 +20,41 @@ namespace
 
 		LUMIX_EXPECT_TRUE(hash_table.empty());
 	};
+
+	void UT_clear(const char* params)
+	{
+		Lumix::DefaultAllocator allocator;
+		Lumix::HashMap<int32_t, int32_t> hash_table(allocator);
+
+		LUMIX_EXPECT_TRUE(hash_table.empty());
+
+		const size_t COUNT = 20;
+
+		for (size_t i = 0; i < COUNT; i++)
+		{
+			hash_table.insert(i, i);
+		}
+
+		for (size_t i = 0; i < COUNT; i++)
+		{
+			LUMIX_EXPECT_EQ(hash_table[i], i);
+		}
+
+		LUMIX_EXPECT_FALSE(hash_table.empty());
+
+		hash_table.clear();
+
+		LUMIX_EXPECT_TRUE(hash_table.empty());
+
+		hash_table.rehash(8);
+
+		for (size_t i = 0; i < COUNT; i++)
+		{
+			hash_table.insert(i, i);
+		}
+	};
 }
 
 REGISTER_TEST("unit_tests/core/hash_map/insert", UT_insert, "")
 REGISTER_TEST("unit_tests/core/hash_map/array", UT_array, "")
+REGISTER_TEST("unit_tests/core/hash_map/clear", UT_clear, "")
