121e61c86d04a9acaa3599e60cbda97fc2f1d0a1
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-12-30 13:21:23 +0100

    improved framebuffer for deffered rendering

--- src/graphics/frame_buffer.h
+++ src/graphics/frame_buffer.h
@@ -13,33 +13,13 @@ namespace Lumix
 class FrameBuffer
 {
 	public:
-		enum RenderBuffers
-		{
-			DIFFUSE,
-			POSITION,
-			NORMAL,
-			DEPTH,
-			RENDERBUFFERS_COUNT
-		};
-
-		enum RenderBufferBits
-		{
-			DIFFUSE_BIT = 1 << DIFFUSE,
-			POSITION_BIT = 1 << POSITION,
-			NORMAL_BIT = 1 << NORMAL,
-			DEPTH_BIT = 1 << DEPTH
-		};
-
-	public:
-		FrameBuffer(int width, int height, int render_buffers, const char* name);
+		FrameBuffer(int width, int height, int color_buffers_count, bool is_depth_buffer, const char* name);
 		~FrameBuffer();
 		
 		GLuint getId() const { return m_id; }
-		GLuint getDiffuseTexture() const { return m_textures[DIFFUSE]; }
-		GLuint getPositionTexture() const { return m_textures[POSITION]; }
-		GLuint getNormalTexture() const { return m_textures[NORMAL]; }
-		GLuint getDepthTexture() const { return m_textures[DEPTH]; }
-		GLuint getTexture(RenderBuffers render_buffer) { return m_textures[render_buffer]; }
+		GLuint getTexture(int index) const { return m_textures[index]; }
+		GLuint getColorTexture(int index) const { return m_textures[index]; }
+		GLuint getDepthTexture() const { ASSERT(m_is_depth_buffer); return m_textures[m_color_buffers_count]; }
 		void bind();
 		int getWidth() const { return m_width; }
 		int getHeight() const { return m_height; }
@@ -47,12 +27,15 @@ class FrameBuffer
 		static void unbind();
 
 	private:
+		static const int MAX_RENDERBUFFERS = 16;
+
 		StackAllocator<64> m_name_allocator;
 		string m_name;
-		GLuint m_textures[RENDERBUFFERS_COUNT];
-		GLuint m_renderbuffers[RENDERBUFFERS_COUNT];
+		GLuint m_textures[MAX_RENDERBUFFERS];
+		GLuint m_renderbuffers[MAX_RENDERBUFFERS];
 		GLuint m_id;
-		int m_render_buffers;
+		int m_color_buffers_count;
+		bool m_is_depth_buffer;
 		int m_width;
 		int m_height;
 };
