7f976252ed7f41055e13f2790d2cc9072e62c7f2
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-02-18 21:42:45 +0100

    deferred point light loop

--- src/graphics/render_scene.h
+++ src/graphics/render_scene.h
@@ -133,6 +133,7 @@ namespace Lumix
 			virtual void setRenderableLayer(Component cmp, const int32_t& layer) = 0;
 			virtual void setRenderablePath(Component cmp, const string& path) = 0;
 			virtual void setRenderableScale(Component cmp, float scale) = 0;
+			virtual Entity getPointLightEntity(Component cmp) = 0;
 			virtual void getPointLights(const Frustum& frustum, Array<Component>& lights) = 0;
 			virtual void getPointLightInfluencedGeometry(const Component& light_cmp, const Frustum& frustum, Array<RenderableInfo>& infos, int64_t layer_mask) = 0;
 			virtual void getRenderableInfos(const Frustum& frustum, Array<RenderableInfo>& infos, int64_t layer_mask) = 0;
