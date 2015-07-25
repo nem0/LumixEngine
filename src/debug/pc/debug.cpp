9c91d0a9999f868eca203fe39a47d9e4172c81b3
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-07-25 02:22:51 +0200

    platform independent engine.cpp - closes #302 bgfx callbacks - closes #491

--- src/debug/pc/debug.cpp
+++ src/debug/pc/debug.cpp
@@ -15,53 +15,74 @@ namespace Lumix
 static char g_base_path[MAX_PATH];
 
 
-BOOL SendFile(LPCSTR lpszSubject, LPCSTR lpszTo, 
-	  LPCSTR lpszName, LPCSTR lpszText, 
-	  LPCSTR lpszFullFileName)
+namespace Debug
+{
+
+
+void debugOutput(const char* message)
+{
+	OutputDebugString(message);
+}
+
+
+void debugBreak()
+{
+	DebugBreak();
+}
+
+
+} // namespace Debug
+
+
+BOOL SendFile(LPCSTR lpszSubject,
+			  LPCSTR lpszTo,
+			  LPCSTR lpszName,
+			  LPCSTR lpszText,
+			  LPCSTR lpszFullFileName)
 {
 	HINSTANCE hMAPI = ::LoadLibrary("mapi32.dll");
 	if (!hMAPI)
 		return FALSE;
-	LPMAPISENDMAIL lpfnMAPISendMail = 
+	LPMAPISENDMAIL lpfnMAPISendMail =
 		(LPMAPISENDMAIL)::GetProcAddress(hMAPI, "MAPISendMail");
 
-	char szDrive[_MAX_DRIVE] = { 0 };
-	char szDir[_MAX_DIR] = { 0 };
-	char szName[_MAX_FNAME] = { 0 };
-	char szExt[_MAX_EXT] = { 0 };
+	char szDrive[_MAX_DRIVE] = {0};
+	char szDir[_MAX_DIR] = {0};
+	char szName[_MAX_FNAME] = {0};
+	char szExt[_MAX_EXT] = {0};
 	_splitpath_s(lpszFullFileName, szDrive, szDir, szName, szExt);
 
-	char szFileName[MAX_PATH] = { 0 };
+	char szFileName[MAX_PATH] = {0};
 	strcat_s(szFileName, szName);
 	strcat_s(szFileName, szExt);
 
-	char szFullFileName[MAX_PATH] = { 0 };
+	char szFullFileName[MAX_PATH] = {0};
 	strcat_s(szFullFileName, lpszFullFileName);
 
-	MapiFileDesc MAPIfile = { 0 };
+	MapiFileDesc MAPIfile = {0};
 	ZeroMemory(&MAPIfile, sizeof(MapiFileDesc));
 	MAPIfile.nPosition = 0xFFFFFFFF;
 	MAPIfile.lpszPathName = szFullFileName;
 	MAPIfile.lpszFileName = szFileName;
 
-	char szTo[MAX_PATH] = { 0 };
+	char szTo[MAX_PATH] = {0};
 	strcat_s(szTo, lpszTo);
 
-	char szNameTo[MAX_PATH] = { 0 };
+	char szNameTo[MAX_PATH] = {0};
 	strcat_s(szNameTo, lpszName);
 
-	MapiRecipDesc recipient = { 0 };
+	MapiRecipDesc recipient = {0};
 	recipient.ulRecipClass = MAPI_TO;
 	recipient.lpszAddress = szTo;
 	recipient.lpszName = szNameTo;
 
-	char szSubject[MAX_PATH] = { 0 };
+	char szSubject[MAX_PATH] = {0};
 	strcat_s(szSubject, lpszSubject);
 
-	char szText[MAX_PATH] = { 0 };
+	char szText[MAX_PATH] = {0};
 	strcat_s(szText, lpszText);
 
-	MapiMessage MAPImsg = { 0 };
+	MapiMessage MAPImsg = {0};
 	MAPImsg.lpszSubject = szSubject;
 	MAPImsg.lpRecips = &recipient;
 	MAPImsg.nRecipCount = 1;
@@ -76,35 +97,69 @@ BOOL SendFile(LPCSTR lpszSubject, LPCSTR lpszTo,
 }
 
 
-static LONG WINAPI unhandledExceptionHandler(LPEXCEPTION_POINTERS info)  
-{  
+static LONG WINAPI unhandledExceptionHandler(LPEXCEPTION_POINTERS info)
+{
 	HANDLE process = GetCurrentProcess();
 	DWORD process_id = GetProcessId(process);
 	char filename[MAX_PATH];
 	sprintf(filename, "%s/minidump.dmp", g_base_path);
 	char* c = filename;
-	while(*c)
+	while (*c)
 	{
-		if(*c == '/')
+		if (*c == '/')
 			*c = '\\';
 		++c;
 	}
-	HANDLE file = CreateFile(filename, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	MINIDUMP_TYPE minidump_type = (MINIDUMP_TYPE)(/*MiniDumpWithFullMemory | MiniDumpWithFullMemoryInfo |*/ MiniDumpFilterMemory | MiniDumpWithHandleData | MiniDumpWithThreadInfo | MiniDumpWithUnloadedModules); 
+	HANDLE file = CreateFile(filename,
+							 GENERIC_WRITE,
+							 0,
+							 NULL,
+							 CREATE_ALWAYS,
+							 FILE_ATTRIBUTE_NORMAL,
+							 NULL);
+	MINIDUMP_TYPE minidump_type = (MINIDUMP_TYPE)(
+		/*MiniDumpWithFullMemory | MiniDumpWithFullMemoryInfo |*/ MiniDumpFilterMemory |
+		MiniDumpWithHandleData | MiniDumpWithThreadInfo |
+		MiniDumpWithUnloadedModules);
 
-	MINIDUMP_EXCEPTION_INFORMATION minidump_exception_info; 
-	minidump_exception_info.ThreadId = GetCurrentThreadId(); 
-	minidump_exception_info.ExceptionPointers = info; 
-	minidump_exception_info.ClientPointers = FALSE; 
+	MINIDUMP_EXCEPTION_INFORMATION minidump_exception_info;
+	minidump_exception_info.ThreadId = GetCurrentThreadId();
+	minidump_exception_info.ExceptionPointers = info;
+	minidump_exception_info.ClientPointers = FALSE;
 
-	MiniDumpWriteDump(process, process_id, file, minidump_type, info ? &minidump_exception_info : NULL, NULL, NULL);
+	MiniDumpWriteDump(process,
+					  process_id,
+					  file,
+					  minidump_type,
+					  info ? &minidump_exception_info : NULL,
+					  NULL,
+					  NULL);
 	CloseHandle(file);
 
-	SendFile("Lumix Studio crash", "SMTP:mikulas.florek@gamedev.sk", "Lumix Studio", "Lumix Studio crashed, minidump attached", filename);
+	SendFile("Lumix Studio crash",
+			 "SMTP:mikulas.florek@gamedev.sk",
+			 "Lumix Studio",
+			 "Lumix Studio crashed, minidump attached",
+			 filename);
 
-	minidump_type = (MINIDUMP_TYPE)(MiniDumpWithFullMemory | MiniDumpWithFullMemoryInfo | MiniDumpFilterMemory | MiniDumpWithHandleData | MiniDumpWithThreadInfo | MiniDumpWithUnloadedModules); 
-	file = CreateFile("fulldump.dmp", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	MiniDumpWriteDump(process, process_id, file, minidump_type, info ? &minidump_exception_info : NULL, NULL, NULL);
+	minidump_type =
+		(MINIDUMP_TYPE)(MiniDumpWithFullMemory | MiniDumpWithFullMemoryInfo |
+						MiniDumpFilterMemory | MiniDumpWithHandleData |
+						MiniDumpWithThreadInfo | MiniDumpWithUnloadedModules);
+	file = CreateFile("fulldump.dmp",
+					  GENERIC_WRITE,
+					  0,
+					  NULL,
+					  CREATE_ALWAYS,
+					  FILE_ATTRIBUTE_NORMAL,
+					  NULL);
+	MiniDumpWriteDump(process,
+					  process_id,
+					  file,
+					  minidump_type,
+					  info ? &minidump_exception_info : NULL,
+					  NULL,
+					  NULL);
 	CloseHandle(file);
 
 	return EXCEPTION_CONTINUE_SEARCH;
@@ -117,7 +172,6 @@ void installUnhandledExceptionHandler(const char* base_path)
 	copyString(g_base_path, sizeof(g_base_path), base_path);
 	SetUnhandledExceptionFilter(unhandledExceptionHandler);
 }
-
 
 
 } // namespace Lumix
