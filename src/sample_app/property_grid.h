6f997ffb9f5fd5951435b8733c0abb91c81d0351
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-09-21 12:59:06 +0200

    missing files

---
+++ src/sample_app/property_grid.h
@@ -0,0 +1,41 @@
+#pragma once
+
+
+#include "universe/component.h"
+
+
+namespace Lumix
+{
+	class IArrayDescriptor;
+	class IPropertyDescriptor;
+	class WorldEditor;
+}
+
+
+class AssetBrowser;
+
+
+class PropertyGrid
+{
+	public:
+		PropertyGrid(Lumix::WorldEditor& editor, AssetBrowser& asset_browser);
+		~PropertyGrid();
+
+		void onGui();
+	
+	public:
+		bool m_is_opened;
+
+	private:
+		void showProperty(Lumix::IPropertyDescriptor& desc, int index, Lumix::ComponentUID cmp);
+		void showArrayProperty(Lumix::ComponentUID cmp, Lumix::IArrayDescriptor& desc);
+		void showComponentProperties(Lumix::ComponentUID cmp);
+		void showCoreProperties(Lumix::Entity entity);
+		const char* getComponentTypeName(Lumix::ComponentUID cmp) const;
+		bool getResourcePath(char* buf, int max_size, uint32_t resource_type);
+
+	private:
+		Lumix::WorldEditor& m_editor;
+		AssetBrowser& m_asset_browser;
+		class TerrainEditor* m_terrain_editor;
+};
