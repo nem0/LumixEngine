4e33986a2a110e9bebb9514a29763810eaefea96
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-09-30 00:48:52 +0200

    asset browser - some user-friendly improvements

--- src/studio/asset_browser.cpp
+++ src/studio/asset_browser.cpp
@@ -11,16 +11,20 @@
 #include "core/resource_manager_base.h"
 #include "core/string.h"
 #include "core/system.h"
+#include "editor/ieditor_command.h"
 #include "editor/world_editor.h"
 #include "engine/engine.h"
+#include "engine/iplugin.h"
 #include "file_system_watcher.h"
 #include "lua_script/lua_script_manager.h"
 #include "metadata.h"
 #include "ocornut-imgui/imgui.h"
 #include "renderer/material.h"
 #include "renderer/model.h"
+#include "renderer/render_scene.h"
 #include "renderer/shader.h"
 #include "renderer/texture.h"
+#include "universe/universe.h"
 #include "utils.h"
 
 
@@ -192,10 +196,13 @@ void AssetBrowser::selectResource(Lumix::Resource* resource)
 
 void AssetBrowser::selectResource(const Lumix::Path& resource)
 {
-	selectResource(m_editor.getEngine()
-					   .getResourceManager()
-					   .get(getResourceType(resource.c_str()))
-					   ->load(resource));
+	char ext[30];
+	Lumix::PathUtils::getExtension(ext, Lumix::lengthOf(ext), resource.c_str());
+	if (strcmp(ext, "unv") == 0) return;
+
+	auto& manager = m_editor.getEngine().getResourceManager();
+	auto* resource_manager = manager.get(getResourceType(resource.c_str()));
+	if (resource_manager) selectResource(resource_manager->load(resource));
 }
 
 
@@ -272,6 +279,8 @@ void AssetBrowser::onGuiMaterial()
 	auto* material = static_cast<Lumix::Material*>(m_selected_resource);
 
 	if (ImGui::Button("Save")) saveMaterial(material);
+	ImGui::SameLine();
+	if (ImGui::Button("Open in external editor")) openInExternalEditor(material);
 
 	bool b;
 	if (material->hasAlphaCutoutDefine())
@@ -358,6 +367,10 @@ void AssetBrowser::onGuiTexture()
 	ImGui::LabelText("BPP", "%d", texture->getBytesPerPixel());
 	m_texture_handle = texture->getTextureHandle();
 	ImGui::Image(&m_texture_handle, ImVec2(200, 200));
+	if (ImGui::Button("Open"))
+	{
+		openInExternalEditor(m_selected_resource);
+	}
 }
 
 
@@ -387,6 +400,19 @@ void AssetBrowser::onGuiLuaScript()
 		file->write(m_text_buffer, strlen(m_text_buffer));
 		fs.close(*file);
 	}
+	ImGui::SameLine();
+	if (ImGui::Button("Open in external editor"))
+	{
+		openInExternalEditor(m_selected_resource);
+	}
+}
+
+
+void AssetBrowser::openInExternalEditor(Lumix::Resource* resource)
+{
+	StringBuilder<Lumix::MAX_PATH_LENGTH> path(m_editor.getBasePath());
+	path << "/" << resource->getPath().c_str();
+	Lumix::shellExecuteOpen(path);
 }
 
 
@@ -431,9 +457,143 @@ void AssetBrowser::onGuiShader()
 }
 
 
+class InsertMeshCommand : public Lumix::IEditorCommand
+{
+public:
+	InsertMeshCommand(Lumix::WorldEditor& editor);
+	InsertMeshCommand(Lumix::WorldEditor& editor, const Lumix::Vec3& position, const Lumix::Path& mesh_path);
+
+	virtual void serialize(Lumix::JsonSerializer& serializer) override;
+	virtual void deserialize(Lumix::JsonSerializer& serializer) override;
+	virtual void execute() override;
+	virtual void undo() override;
+	virtual uint32_t getType() override;
+	virtual bool merge(IEditorCommand&);
+	Lumix::Entity getEntity() const { return m_entity; }
+
+private:
+	Lumix::Vec3 m_position;
+	Lumix::Path m_mesh_path;
+	Lumix::Entity m_entity;
+	Lumix::WorldEditor& m_editor;
+};
+
+
+
+static const uint32_t RENDERABLE_HASH = Lumix::crc32("renderable");
+
+
+InsertMeshCommand::InsertMeshCommand(Lumix::WorldEditor& editor)
+	: m_editor(editor)
+{
+}
+
+
+InsertMeshCommand::InsertMeshCommand(Lumix::WorldEditor& editor,
+	const Lumix::Vec3& position,
+	const Lumix::Path& mesh_path)
+	: m_mesh_path(mesh_path)
+	, m_position(position)
+	, m_editor(editor)
+{
+}
+
+
+void InsertMeshCommand::serialize(Lumix::JsonSerializer& serializer)
+{
+	serializer.serialize("path", m_mesh_path.c_str());
+	serializer.beginArray("pos");
+	serializer.serializeArrayItem(m_position.x);
+	serializer.serializeArrayItem(m_position.y);
+	serializer.serializeArrayItem(m_position.z);
+	serializer.endArray();
+}
+
+
+void InsertMeshCommand::deserialize(Lumix::JsonSerializer& serializer)
+{
+	char path[Lumix::MAX_PATH_LENGTH];
+	serializer.deserialize("path", path, sizeof(path), "");
+	m_mesh_path = path;
+	serializer.deserializeArrayBegin("pos");
+	serializer.deserializeArrayItem(m_position.x, 0);
+	serializer.deserializeArrayItem(m_position.y, 0);
+	serializer.deserializeArrayItem(m_position.z, 0);
+	serializer.deserializeArrayEnd();
+}
+
+
+void InsertMeshCommand::execute()
+{
+	Lumix::Engine& engine = m_editor.getEngine();
+	Lumix::Universe* universe = m_editor.getUniverse();
+	m_entity = universe->createEntity(Lumix::Vec3(0, 0, 0), Lumix::Quat(0, 0, 0, 1));
+	universe->setPosition(m_entity, m_position);
+	const Lumix::Array<Lumix::IScene*>& scenes = m_editor.getScenes();
+	Lumix::ComponentIndex cmp;
+	Lumix::IScene* scene = nullptr;
+	for (int i = 0; i < scenes.size(); ++i)
+	{
+		cmp = scenes[i]->createComponent(RENDERABLE_HASH, m_entity);
+
+		if (cmp >= 0)
+		{
+			scene = scenes[i];
+			break;
+		}
+	}
+	if (cmp >= 0)
+	{
+		char rel_path[Lumix::MAX_PATH_LENGTH];
+		m_editor.getRelativePath(rel_path, Lumix::MAX_PATH_LENGTH, m_mesh_path.c_str());
+		static_cast<Lumix::RenderScene*>(scene)->setRenderablePath(cmp, rel_path);
+	}
+}
+
+
+void InsertMeshCommand::undo()
+{
+	const Lumix::WorldEditor::ComponentList& cmps =
+		m_editor.getComponents(m_entity);
+	for (int i = 0; i < cmps.size(); ++i)
+	{
+		cmps[i].scene->destroyComponent(cmps[i].index, cmps[i].type);
+	}
+	m_editor.getUniverse()->destroyEntity(m_entity);
+	m_entity = Lumix::INVALID_ENTITY;
+}
+
+
+uint32_t InsertMeshCommand::getType()
+{
+	static const uint32_t type = Lumix::crc32("insert_mesh");
+	return type;
+}
+
+
+bool InsertMeshCommand::merge(IEditorCommand&)
+{
+	return false;
+}
+
+
+static void insertInScene(Lumix::WorldEditor& editor, Lumix::Model* model)
+{
+	auto* command = LUMIX_NEW(editor.getAllocator(), InsertMeshCommand)(
+		editor, editor.getCameraRaycastHit(), model->getPath());
+
+	editor.executeCommand(command);
+}
+
+
 void AssetBrowser::onGuiModel()
 {
 	auto* model = static_cast<Lumix::Model*>(m_selected_resource);
+	if (ImGui::Button("Insert in scene"))
+	{
+		insertInScene(m_editor, model);
+	}
+
 	ImGui::LabelText("Bone count", "%d", model->getBoneCount());
 	if (model->getBoneCount() > 0) ImGui::CollapsingHeader("Bones");
 	for (int i = 0; i < model->getBoneCount(); ++i)
