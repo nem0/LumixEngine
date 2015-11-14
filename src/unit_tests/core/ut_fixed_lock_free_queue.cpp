e049e3b03e045788798a1b6db28b6cb6bd65fa96
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-11-14 10:37:46 +0100

    removed unit test which is not relevant anymore

--- src/unit_tests/core/ut_fixed_lock_free_queue.cpp
+++ src/unit_tests/core/ut_fixed_lock_free_queue.cpp
@@ -57,7 +57,7 @@ namespace
 		int32 m_sum;
 	};
 
-	void UT_fixed_lock_queue_non_trivial_constructors(const char* params)
+	void UT_fixed_lock_queue(const char* params)
 	{
 		Lumix::DefaultAllocator allocator;
 		Queue queue;
@@ -83,69 +83,6 @@ namespace
 
 		LUMIX_EXPECT_EQ(RUN_COUNT, testTaskConsumer.getSum());
 	};
-
-	class TestTaskPodConsumer : public Lumix::MT::Task
-	{
-	public:
-		TestTaskPodConsumer(Queue* queue, Lumix::IAllocator& allocator)
-			: Lumix::MT::Task(allocator)
-			, m_queue(queue)
-			, m_sum(0)
-		{}
-
-		~TestTaskPodConsumer()
-		{}
-
-		int task()
-		{
-			while (!m_queue->isAborted())
-			{
-				Test* test = m_queue->pop(true);
-				if (NULL == test)
-					break;
-
-				m_sum += test->value;
-				test->value++;
-
-				m_queue->dealoc(test);
-			}
-			return 0;
-		}
-
-		int32 getSum() { return m_sum; }
-
-	private:
-		Queue* m_queue;
-		int32 m_sum;
-	};
-
-	void UT_fixed_lock_queue_trivial_constructors(const char* params)
-	{
-		Lumix::DefaultAllocator allocator;
-		Queue queue;
-		TestTaskPodConsumer testTaskPodConsumer(&queue, allocator);
-		testTaskPodConsumer.create("TestTaskPodConsumer_Task");
-		testTaskPodConsumer.run();
-
-		const int RUN_COUNT = 512;
-
-		for (int i = 0; i < RUN_COUNT; i++)
-		{
-			Test* test = queue.alloc(true);
-			queue.push(test, true);
-		}
-
-		while (!queue.isEmpty())
-		{
-			Lumix::MT::yield();
-		}
-
-		queue.abort();
-		testTaskPodConsumer.destroy();
-
-		LUMIX_EXPECT_EQ(8448, testTaskPodConsumer.getSum());
-	};
 }
 
-REGISTER_TEST("unit_tests/core/multi_thread/fixed_lock_queue_non_trivial_constructors", UT_fixed_lock_queue_non_trivial_constructors, "");
-REGISTER_TEST("unit_tests/core/multi_thread/fixed_lock_queue_trivial_constructors", UT_fixed_lock_queue_trivial_constructors, "");
+REGISTER_TEST("unit_tests/core/multi_thread/fixed_lock_queue", UT_fixed_lock_queue, "");
