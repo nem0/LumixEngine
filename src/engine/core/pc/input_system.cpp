b04d89b3f572b864b0d3c1491e04da763094624c
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-11-17 22:39:10 +0100

    Input system pimpl refactor

--- src/engine/core/pc/input_system.cpp
+++ src/engine/core/pc/input_system.cpp
@@ -11,19 +11,83 @@
 namespace Lumix
 {
 
-	struct InputSystemImpl
+	struct InputSystemImpl : public InputSystem
 	{
 		InputSystemImpl(IAllocator& allocator)
 			: m_actions(allocator)
 			, m_allocator(allocator)
 			, m_is_enabled(false)
+			, m_mouse_rel_x(0)
+			, m_mouse_rel_y(0)
 		{}
+
+
+		void enable(bool enabled) override
+		{
+			m_is_enabled = enabled;
+		}
+
+
+		void update(float) override
+		{
+			m_mouse_rel_x = 0;
+			m_mouse_rel_y = 0;
+		}
+
+
+		void injectMouseXMove(float value) override
+		{
+			m_mouse_rel_x = value;
+		}
+
+
+		void injectMouseYMove(float value) override
+		{
+			m_mouse_rel_y = value;
+		}
+
+
+		void addAction(uint32 action, InputType type, int key) override
+		{
+			InputSystemImpl::Action value;
+			value.key = key;
+			value.type = type;
+			m_actions[action] = value;
+		}
+
+
+		float getActionValue(uint32 action) override
+		{
+			if (!m_is_enabled) return 0;
+			InputSystemImpl::Action value;
+			if (m_actions.find(action, value))
+			{
+				switch (value.type)
+				{
+				case InputType::PRESSED:
+					return (GetAsyncKeyState(value.key) >> 8) ? 1.0f : 0;
+					break;
+				case InputType::DOWN:
+					return GetAsyncKeyState(value.key) & 1 ? 1.0f : 0;
+					break;
+				case InputType::MOUSE_X:
+					return m_mouse_rel_x;
+					break;
+				case InputType::MOUSE_Y:
+					return m_mouse_rel_y;
+					break;
+				};
+			}
+			return -1;
+		}
+
 
 		struct Action
 		{
 			InputSystem::InputType type;
 			int key;
 		};
+
 
 		IAllocator& m_allocator;
 		AssociativeArray<uint32, Action> m_actions;
@@ -33,89 +97,17 @@ namespace Lumix
 	};
 
 
-	void InputSystem::enable(bool enabled)
+	InputSystem* InputSystem::create(IAllocator& allocator)
 	{
-		m_impl->m_is_enabled = enabled;
+		return LUMIX_NEW(allocator, InputSystemImpl)(allocator);
 	}
 
 
-	void InputSystem::update(float)
+	void InputSystem::destroy(InputSystem& system)
 	{
-		m_impl->m_mouse_rel_x = 0;
-		m_impl->m_mouse_rel_y = 0;
+		auto* impl = static_cast<InputSystemImpl*>(&system);
+		LUMIX_DELETE(impl->m_allocator, impl);
 	}
 
 
-	InputSystem::InputSystem()
-		: m_impl(nullptr)
-	{
-	}
-
-
-	bool InputSystem::create(IAllocator& allocator)
-	{
-		m_impl = LUMIX_NEW(allocator, InputSystemImpl)(allocator);
-		m_impl->m_mouse_rel_x = 0;
-		m_impl->m_mouse_rel_y = 0;
-		return true;
-	}
-
-
-	void InputSystem::destroy()
-	{
-		LUMIX_DELETE(m_impl->m_allocator, m_impl);
-		m_impl = nullptr;
-	}
-
-
-	void InputSystem::injectMouseXMove(float value)
-	{
-		m_impl->m_mouse_rel_x = value;
-	}
-
-
-	void InputSystem::injectMouseYMove(float value)
-	{
-		m_impl->m_mouse_rel_y = value;
-	}
-
-
-	void InputSystem::addAction(uint32 action, InputType type, int key)
-	{
-		InputSystemImpl::Action value;
-		value.key = key;
-		value.type = type;
-		m_impl->m_actions[action] =  value;
-	}
-
-
-	float InputSystem::getActionValue(uint32 action)
-	{
-		if (!m_impl->m_is_enabled)
-		{
-			return 0;
-		}
-		InputSystemImpl::Action value;
-		if(m_impl->m_actions.find(action, value))
-		{
-			switch(value.type)
-			{
-				case InputType::PRESSED:
-					return (GetAsyncKeyState(value.key) >> 8) ? 1.0f : 0;
-					break;
-				case InputType::DOWN:
-					return GetAsyncKeyState(value.key) & 1 ? 1.0f : 0;
-					break;
-				case InputType::MOUSE_X:
-					return m_impl->m_mouse_rel_x;
-					break;
-				case InputType::MOUSE_Y:
-					return m_impl->m_mouse_rel_y;
-					break;
-			};
-		}
-		return -1;
-	}
-
-
-} // ~ namespace Lumix
+} // namespace Lumix
