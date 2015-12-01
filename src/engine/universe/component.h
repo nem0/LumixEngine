15e544c1df8687264bea504f1f5b9ebbc4c6be95
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-12-02 00:03:46 +0100

    cleanup

--- src/engine/universe/component.h
+++ src/engine/universe/component.h
@@ -16,11 +16,15 @@ struct LUMIX_ENGINE_API ComponentUID final
 
 	typedef uint32 Type;
 
-	ComponentUID() { index = -1; scene = nullptr; entity = -1; type = 0; }
-	ComponentUID(Entity _entity,
-				 uint32 _type,
-				 IScene* _scene,
-				 int _index)
+	ComponentUID()
+	{
+		index = -1;
+		scene = nullptr;
+		entity = -1;
+		type = 0;
+	}
+
+	ComponentUID(Entity _entity, uint32 _type, IScene* _scene, int _index)
 		: entity(_entity)
 		, type(_type)
 		, scene(_scene)
@@ -38,16 +42,6 @@ struct LUMIX_ENGINE_API ComponentUID final
 	bool operator==(const ComponentUID& rhs) const
 	{
 		return type == rhs.type && scene == rhs.scene && index == rhs.index;
-	}
-	bool operator!=(const ComponentUID& rhs) const
-	{
-		return type != rhs.type || scene != rhs.scene || index != rhs.index;
-	}
-	bool operator<(const ComponentUID& rhs) const
-	{
-		ASSERT(type == rhs.type);
-		ASSERT(scene == rhs.scene);
-		return index < rhs.index;
 	}
 	bool isValid() const { return index >= 0; }
 };
