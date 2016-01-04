d1b6eed9b5c33cbac7887e076b2e22a537e95544
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-01-04 14:07:11 +0100

    imgui docking in standalone files

---
+++ src/studio_lib/imgui/imgui_dock.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+namespace Lumix { namespace FS { class OsFile; } }
+
+
+struct lua_State;
+
+
+namespace ImGui
+{
+
+IMGUI_API void ShutdownDock();
+IMGUI_API bool BeginDock(const char* label, bool* opened = nullptr, ImGuiWindowFlags extra_flags = 0);
+IMGUI_API void EndDock();
+IMGUI_API void SaveDock(Lumix::FS::OsFile& file);
+IMGUI_API void LoadDock(lua_State* L);
+
+
+} // namespace ImGui
