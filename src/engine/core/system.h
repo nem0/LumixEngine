875143155e723308a1b7d848c40a17f67021ef47
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-01-05 22:32:24 +0100

    refactor - some functions moved from engine to studio_lib

--- src/engine/core/system.h
+++ src/engine/core/system.h
@@ -6,20 +6,9 @@
 
 namespace Lumix
 {
-
-	class IAllocator;
-
-	LUMIX_ENGINE_API bool deleteFile(const char* path);
-	LUMIX_ENGINE_API bool moveFile(const char* from, const char* to);
 	LUMIX_ENGINE_API bool copyFile(const char* from, const char* to);
-	LUMIX_ENGINE_API bool fileExists(const char* path);
-	LUMIX_ENGINE_API bool dirExists(const char* path);
-	LUMIX_ENGINE_API uint64 getLastModified(const char* file);
 	LUMIX_ENGINE_API void messageBox(const char* text);
-	LUMIX_ENGINE_API bool makePath(const char* path);
-
 	LUMIX_ENGINE_API bool getCommandLine(char* output, int max_size);
-
 	LUMIX_ENGINE_API void* loadLibrary(const char* path);
 	LUMIX_ENGINE_API void unloadLibrary(void* handle);
 	LUMIX_ENGINE_API void* getLibrarySymbol(void* handle, const char* name);
--- src/studio_lib/platform_interface.h
+++ src/studio_lib/platform_interface.h
@@ -104,6 +104,13 @@ namespace PlatformInterface
 	bool getOpenDirectory(char* out, int max_size);
 	bool shellExecuteOpen(const char* path);
 
+	bool deleteFile(const char* path);
+	bool moveFile(const char* from, const char* to);
+	bool fileExists(const char* path);
+	bool dirExists(const char* path);
+	Lumix::uint64 getLastModified(const char* file);
+	bool makePath(const char* path);
+
 	struct Process;
 
 	Process* createProcess(const char* cmd, const char* args, Lumix::IAllocator& allocator);
