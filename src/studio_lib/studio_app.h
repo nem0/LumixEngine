e5e6ea1b34c5ab666f503516094c6b054c80a249
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-01-10 22:51:32 +0100

    lua wrapper - wrap method

--- src/studio_lib/studio_app.h
+++ src/studio_lib/studio_app.h
@@ -34,6 +34,7 @@ public:
 	virtual void addPlugin(IPlugin& plugin) = 0;
 	virtual void removePlugin(IPlugin& plugin) = 0;
 	virtual int getExitCode() const = 0;
+	virtual void runScript(const char* src, const char* script_name) = 0;
 
 	virtual ~StudioApp() {}
 	virtual void run() = 0;
