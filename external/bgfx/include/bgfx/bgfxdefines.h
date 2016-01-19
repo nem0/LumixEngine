d76fbc00fb96337f9ca715d38ebf4907ad3fb90e
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-01-19 22:40:28 +0100

    3rd party libraries updated

--- external/bgfx/include/bgfx/bgfxdefines.h
+++ external/bgfx/include/bgfx/bgfxdefines.h
@@ -1,12 +1,12 @@
 /*
- * Copyright 2011-2015 Branimir Karadzic. All rights reserved.
- * License: http://www.opensource.org/licenses/BSD-2-Clause
+ * Copyright 2011-2016 Branimir Karadzic. All rights reserved.
+ * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
  */
 
 #ifndef BGFX_DEFINES_H_HEADER_GUARD
 #define BGFX_DEFINES_H_HEADER_GUARD
 
-#define BGFX_API_VERSION UINT32_C(2)
+#define BGFX_API_VERSION UINT32_C(5)
 
 ///
 #define BGFX_STATE_RGB_WRITE             UINT64_C(0x0000000000000001) //!< Enable RGB write.
@@ -300,7 +300,7 @@
 #define BGFX_TEXTURE_RT_MSAA_X16         UINT32_C(0x00005000) //!<
 #define BGFX_TEXTURE_RT_MSAA_SHIFT       12                   //!<
 #define BGFX_TEXTURE_RT_MSAA_MASK        UINT32_C(0x00007000) //!<
-#define BGFX_TEXTURE_RT_BUFFER_ONLY      UINT32_C(0x00008000) //!<
+#define BGFX_TEXTURE_RT_WRITE_ONLY       UINT32_C(0x00008000) //!<
 #define BGFX_TEXTURE_RT_MASK             UINT32_C(0x0000f000) //!<
 #define BGFX_TEXTURE_COMPARE_LESS        UINT32_C(0x00010000) //!<
 #define BGFX_TEXTURE_COMPARE_LEQUAL      UINT32_C(0x00020000) //!<
@@ -354,6 +354,7 @@
 #define BGFX_RESET_FLIP_AFTER_RENDER     UINT32_C(0x00004000) //!< This flag  specifies where flip occurs. Default behavior is that flip occurs before rendering new frame. This flag only has effect when `BGFX_CONFIG_MULTITHREADED=0`.
 #define BGFX_RESET_SRGB_BACKBUFFER       UINT32_C(0x00008000) //!< Enable sRGB backbuffer.
 #define BGFX_RESET_HIDPI                 UINT32_C(0x00010000) //!< Enable HiDPI rendering.
+#define BGFX_RESET_DEPTH_CLAMP           UINT32_C(0x00020000) //!< Enable depth clamp.
 
 #define BGFX_RESET_RESERVED_SHIFT        31                   //!< Internal bits shift.
 #define BGFX_RESET_RESERVED_MASK         UINT32_C(0x80000000) //!< Internal bits mask.
