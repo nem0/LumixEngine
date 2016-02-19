308dd2b61e93b4a0bb4a17989e7c0fbde510117e
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-02-19 17:13:29 +0100

    script gui WIP

--- src/engine/lumix.h
+++ src/engine/lumix.h
@@ -14,14 +14,14 @@
 
 namespace Lumix
 {
-	typedef char					int8;
-	typedef unsigned char			uint8;
-	typedef short					int16;
-	typedef unsigned short			uint16;
-	typedef int						int32;
-	typedef unsigned int			uint32;
-	typedef long long				int64;
-	typedef unsigned long long		uint64;
+typedef char int8;
+typedef unsigned char uint8;
+typedef short int16;
+typedef unsigned short uint16;
+typedef int int32;
+typedef unsigned int uint32;
+typedef long long int64;
+typedef unsigned long long uint64;
 
 #ifdef _WIN64
 	typedef uint64 uintptr;
