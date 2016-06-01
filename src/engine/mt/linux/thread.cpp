b9d5433ff9a545f3433b8f45558b3c0681252e18
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-06-01 10:37:57 +0200

    linux filesystem watcher #655

--- src/engine/mt/linux/thread.cpp
+++ src/engine/mt/linux/thread.cpp
@@ -13,13 +13,7 @@ namespace MT
 
 void sleep(uint32 milliseconds)
 {
-	if (milliseconds)
-	{
-		timespec t;
-		t.tv_sec = milliseconds / 1000;
-		t.tv_nsec = (milliseconds % 1000) * 1000000;
-		clock_nanosleep(CLOCK_MONOTONIC, 0, &t, nullptr);
-	}
+	if (milliseconds) usleep(useconds_t(milliseconds * 1000));
 }
 
 
