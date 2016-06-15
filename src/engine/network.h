dc5828110c5aa01c231143bf632b68b0a400e0e6
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-06-15 18:34:13 +0200

    fixed possible crash with 3 keys shortcuts

--- src/engine/network.h
+++ src/engine/network.h
@@ -58,24 +58,8 @@ public:
 	}
 	~TCPStream();
 
-	LUMIX_FORCE_INLINE bool read(bool& val) { return read(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool read(uint8& val) { return read(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool read(int8& val) { return read(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool read(uint16& val) { return read(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool read(int16& val) { return read(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool read(uint32& val) { return read(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool read(int32& val) { return read(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool read(uint64& val) { return read(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool read(int64& val) { return read(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool write(bool val) { return write(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool write(uint8 val) { return write(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool write(int8 val) { return write(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool write(uint16 val) { return write(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool write(int16 val) { return write(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool write(uint32 val) { return write(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool write(int32 val) { return write(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool write(uint64 val) { return write(&val, sizeof(val)); }
-	LUMIX_FORCE_INLINE bool write(int64 val) { return write(&val, sizeof(val)); }
+	template <typename T> LUMIX_FORCE_INLINE bool read(T& val) { return read(&val, sizeof(val)); }
+	template <typename T> LUMIX_FORCE_INLINE bool write(T val) { return write(&val, sizeof(val)); }
 
 	bool readString(char* string, uint32 max_size);
 	bool writeString(const char* string);
