57e1bc590ce24b2768ede6728a9740c9be4391e7
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-09-10 01:28:06 +0200

    Fixed copy/paste of scaled entity

--- src/engine/universe/universe.cpp
+++ src/engine/universe/universe.cpp
@@ -119,9 +119,8 @@ bool Universe::hasEntity(Entity entity) const
 
 void Universe::setMatrix(Entity entity, const Matrix& mtx)
 {
-	Quat rot = mtx.getRotation();
-	m_transformations[m_entity_map[entity.index]].position = mtx.getTranslation();
-	m_transformations[m_entity_map[entity.index]].rotation = rot;
+	Transformation& out = m_transformations[m_entity_map[entity.index]];
+	mtx.decompose(out.position, out.rotation, out.scale);
 	entityTransformed().invoke(entity);
 }
 
