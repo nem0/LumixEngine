797147ac5fab07331fa76dedd8cc008a22192e6d
Jakub Lukasik <lukasik.lukasik@gmail.com>
2016-11-10 19:44:04 +0100

    curve handles for sampled function property (#1035)
    * movable handles for curve editor
    
    * curve editor handles
    
    * curve editor handlers WIP
    
    * func to find param of point on curve WIP
    
    * add curve point wip
    
    * a bit closer to bezier segment splitting
    
    * headers and inline files weren't in studio project
    
    * fix by broken merge
    commit of WIP function editor
    
    * still WIP
    
    * finaly kind of working handles
    
    * Fixed: doubled forward declaration
    
    * Revert "a bit closer to bezier segment splitting"
    
    This reverts commit 499edc0834838c1557c4a8e7993607361d35755e.
    
    # Conflicts:
    #	src/editor/property_grid.cpp
    #	src/studio/imgui_user.h
    #	src/studio/imgui_user.inl
    
    * total mess, creating raytracer renderer WIP
    
    * still total mess
    
    * still broken
    
    * Revert "total mess, creating raytracer renderer WIP"
    
    This reverts commit 7c015865dab7348badc4e3f7253b8e71160156ab.
    
    * Added: handles
    
    * Added: handles
    
    * Tweaked: handle shape and size
    
    * Tweaked: handle draging movement
    
    * tweaks
    
    * Maybe final tweaks of simple function editor
    
    * Fixed: formatting tabs/spaces
    
    * last function point cannot be deleted
    first point cannot be moved in x axis
    
    * reverted some strange code change

--- external/imgui/imgui_user.h
+++ external/imgui/imgui_user.h
@@ -11,23 +11,23 @@ namespace ImGui
 
 IMGUI_API void ResetActiveID();
 IMGUI_API int PlotHistogramEx(const char* label,
-    float (*values_getter)(void* data, int idx),
-    void* data,
-    int values_count,
-    int values_offset,
-    const char* overlay_text,
-    float scale_min,
-    float scale_max,
-    ImVec2 graph_size,
-    int selected_index);
+	float(*values_getter)(void* data, int idx),
+	void* data,
+	int values_count,
+	int values_offset,
+	const char* overlay_text,
+	float scale_min,
+	float scale_max,
+	ImVec2 graph_size,
+	int selected_index);
 
 IMGUI_API bool ListBox(const char* label,
-    int* current_item,
-    int scroll_to_item,
-    bool (*items_getter)(void*, int, const char**),
-    void* data,
-    int items_count,
-    int height_in_items);
+	int* current_item,
+	int scroll_to_item,
+	bool(*items_getter)(void*, int, const char**),
+	void* data,
+	int items_count,
+	int height_in_items);
 IMGUI_API bool ColorPicker(float* col, bool alphabar);
 
 IMGUI_API void BringToFront();
@@ -47,15 +47,18 @@ IMGUI_API void NodeSlots(int count, bool input);
 
 struct CurveEditor
 {
-    bool valid;
-    ImVec2 beg_pos;
-    ImVec2 prev_point;
-    ImVec2 prev_tangent;
-    int point_idx;
+	bool valid;
+	ImVec2 beg_pos;
+	ImVec2 graph_size;
+	static const float GRAPH_MARGIN;
+	static const float HEIGHT;
+	ImVec2 inner_bb_min;
+	ImVec2 inner_bb_max;
+	int point_idx;
 };
 
 IMGUI_API CurveEditor BeginCurveEditor(const char* label);
-IMGUI_API bool CurvePoint(ImVec2* point, CurveEditor& editor);
+IMGUI_API bool CurveSegment(ImVec2* point, CurveEditor& editor);
 IMGUI_API void EndCurveEditor(const CurveEditor& editor);
 IMGUI_API bool BeginResizablePopup(const char* str_id, const ImVec2& size_on_first_use);
 IMGUI_API void IntervalGraph(const unsigned long long* value_pairs,
