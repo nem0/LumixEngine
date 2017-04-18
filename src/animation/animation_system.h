40fdfd4e9c2a91f71e06e979a37b9ff7793d5a5e
Mikulas Florek <mikulas.florek@gamedev.sk>
2017-04-18 12:34:44 +0200

    animation system - apply controller set

--- src/animation/animation_system.h
+++ src/animation/animation_system.h
@@ -34,6 +34,7 @@ struct AnimationScene : public IScene
 	virtual int getControllerInputIndex(ComponentHandle cmp, const char* name) const = 0;
 	virtual Entity getSharedControllerParent(ComponentHandle cmp) = 0;
 	virtual void setSharedControllerParent(ComponentHandle cmp, Entity parent) = 0;
+	virtual void applyControllerSet(ComponentHandle cmp, const char* set_name) = 0;
 	virtual void setControllerDefaultSet(ComponentHandle cmp, int set) = 0;
 	virtual int getControllerDefaultSet(ComponentHandle cmp) = 0;
 	virtual Anim::ControllerResource* getControllerResource(ComponentHandle cmp) = 0;
