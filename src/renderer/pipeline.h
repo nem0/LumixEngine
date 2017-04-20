9e37c382d2a9850eff8a189f8c4ec7e65c97cc95
Jakub Lukasik <lukasik.lukasik@gmail.com>
2017-04-20 15:18:08 +0200

    Pipeline interface: added another render(...) for plugins (#1122)
    * i hate git
    
    * getCurrentView and executeCommandBuffer are in pipeline interface
    
    * render interface function
    
    * Reverted: View class definition back to cpp file
    
    * copy/paste typo

--- src/renderer/pipeline.h
+++ src/renderer/pipeline.h
@@ -15,6 +15,9 @@ namespace bgfx
 	struct TransientVertexBuffer;
 	struct TransientIndexBuffer;
 	struct VertexDecl;
+	struct VertexBufferHandle;
+	struct IndexBufferHandle;
+	struct InstanceDataBuffer;
 }
 
 
@@ -30,6 +33,7 @@ class Path;
 class Renderer;
 class RenderScene;
 struct Vec4;
+class Material;
 
 
 struct CommandBufferGenerator
@@ -126,6 +130,12 @@ class LUMIX_RENDERER_API Pipeline
 		virtual float getWaitSubmitTime() const = 0;
 		virtual float getWaitRenderTime() const = 0;
 		virtual void callLuaFunction(const char* func) = 0;
+
+		virtual void render(const bgfx::VertexBufferHandle& vertex_buffer,
+			const bgfx::IndexBufferHandle& index_buffer,
+			const bgfx::InstanceDataBuffer& instance_buffer,
+			int count,
+			Material& material) = 0;
 };
 
 
