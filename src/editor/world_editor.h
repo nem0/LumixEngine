67515bba7975dd0738ec59f5b1ecb08b9f839c3f
Mikulas Florek <mikulas.florek@gamedev.sk>
2017-07-07 21:28:25 +0200

    relative output dirs in import asset dialog

--- src/editor/world_editor.h
+++ src/editor/world_editor.h
@@ -168,6 +168,7 @@ public:
 	virtual void setSideView() = 0;
 	virtual class MeasureTool* getMeasureTool() const = 0;
 	virtual void makeRelative(char* relative, int max_size, const char* absolute) const = 0;
+	virtual void makeAbsolute(char* absolute, int max_size, const char* relative) const = 0;
 
 	virtual void saveUndoStack(const Path& path) = 0;
 	virtual bool executeUndoStack(const Path& path) = 0;
