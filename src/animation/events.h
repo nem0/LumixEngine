b139d45afe8e9f39a51675505380680fa32708e7
Tim Baker <timnbaker@gmail.com>
2017-09-07 10:24:24 -0700

    Added animation enter / exit events.

--- src/animation/events.h
+++ src/animation/events.h
@@ -2,6 +2,7 @@
 
 
 #include "engine/lumix.h"
+#include "engine/array.h"
 
 
 namespace Lumix
@@ -18,6 +19,28 @@ struct EventHeader
 	u32 type;
 	u16 offset;
 	u8 size;
+};
+
+
+struct EnterExitEventHeader
+{
+	u32 type;
+	u16 offset;
+	u8 size;
+};
+
+
+struct EventArray
+{
+	EventArray(IAllocator& allocator)
+		: data(allocator)
+		, count(0)
+	{}
+	void remove(int index);
+	void append(int size, u32 type);
+
+	Array<u8> data;
+	int count;
 };
 
 
