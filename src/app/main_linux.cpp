e269b8e45392ace3b0a72e36eead25d57d3dc252
Mikulas Florek <mikulas.florek@gamedev.sk>
2017-10-04 13:29:39 +0200

    fixed linux

--- src/app/main_linux.cpp
+++ src/app/main_linux.cpp
@@ -165,7 +165,7 @@ struct App
 		m_engine->getPluginManager().load("physics");
 		m_engine->getInputSystem().enable(true);
 		Renderer* renderer = static_cast<Renderer*>(m_engine->getPluginManager().getPlugin("renderer"));
-		m_pipeline = Pipeline::create(*renderer, Path(m_pipeline_path), m_engine->getAllocator());
+		m_pipeline = Pipeline::create(*renderer, Path(m_pipeline_path), "", m_engine->getAllocator());
 		m_pipeline->load();
 
 		while (m_engine->getFileSystem().hasWork())
