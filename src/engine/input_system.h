fafc0a7f822debf6c74a617709a67a585f8d8433
Mikulas Florek <mikulas.florek@gamedev.sk>
2017-10-13 12:27:48 +0200

    input system refactor #1177

--- src/engine/input_system.h
+++ src/engine/input_system.h
@@ -23,6 +23,11 @@ class LUMIX_ENGINE_API InputSystem
 			};
 
 			Type type;
+			int index = 0;
+
+			virtual ~Device() {}
+			virtual void update(float dt) = 0;
+			virtual const char* getName() const = 0;
 		};
 
 		struct ButtonEvent
@@ -67,6 +72,7 @@ class LUMIX_ENGINE_API InputSystem
 		static void destroy(InputSystem& system);
 
 		virtual ~InputSystem() {}
+		virtual IAllocator& getAllocator() = 0;
 		virtual void enable(bool enabled) = 0;
 		virtual void update(float dt) = 0;
 
@@ -74,8 +80,12 @@ class LUMIX_ENGINE_API InputSystem
 		virtual int getEventsCount() const = 0;
 		virtual const Event* getEvents() const = 0;
 
+		virtual void addDevice(Device* device) = 0;
+		virtual void removeDevice(Device* device) = 0;
 		virtual Device* getMouseDevice() = 0;
 		virtual Device* getKeyboardDevice() = 0;
+		virtual int getDevicesCount() const = 0;
+		virtual Device* getDevice(int index) = 0;
 
 		virtual Vec2 getCursorPosition() const = 0;
 		virtual void setCursorPosition(const Vec2& pos) = 0;
---
+++ src/engine/win/controller_device.cpp
@@ -0,0 +1,88 @@
+#include "engine/controller_device.h"
+#include "engine/iallocator.h"
+
+
+#include <windows.h>
+#include <Xinput.h>
+
+
+namespace Lumix
+{
+
+
+typedef decltype(XInputGetState)* XInputGetState_fn_ptr;
+
+
+static struct {
+	int last_checked;
+	bool connected[XUSER_MAX_COUNT];
+	HMODULE lib;
+	XInputGetState_fn_ptr get_state;
+	InputSystem* input;
+	XINPUT_STATE states[XUSER_MAX_COUNT];
+	InputSystem::Device* devices[XUSER_MAX_COUNT];
+} g_controllers;
+
+
+struct XInputControllerDevice : ControllerDevice
+{
+	void update(float dt) override {}
+};
+
+
+void ControllerDevice::init(InputSystem& input_system)
+{
+	for (Device*& dev : g_controllers.devices) dev = nullptr;
+	for (bool& connected : g_controllers.connected) connected = false;
+	
+	g_controllers.input = &input_system;
+	g_controllers.last_checked = 0;
+	g_controllers.lib = LoadLibrary("Xinput9_1_0.dll");
+	if (g_controllers.lib)
+	{
+		g_controllers.get_state = (XInputGetState_fn_ptr)GetProcAddress(g_controllers.lib, "XInputGetState");
+		if (!g_controllers.get_state)
+		{
+			FreeLibrary(g_controllers.lib);
+			g_controllers.lib = nullptr;
+		}
+	}
+}
+
+
+void ControllerDevice::update(float dt)
+{
+	if (!g_controllers.get_state) return;
+
+	for (int i = 0; i < XUSER_MAX_COUNT; ++i)
+	{
+		if (g_controllers.connected[i] || i == g_controllers.last_checked)
+		{
+			auto status = g_controllers.get_state(i, &g_controllers.states[i]);
+			g_controllers.connected[i] = status == ERROR_SUCCESS;
+			if (g_controllers.connected[i] && !g_controllers.devices[i])
+			{
+				XInputControllerDevice* new_device = LUMIX_NEW(g_controllers.input->getAllocator(), XInputControllerDevice);
+				new_device->type = InputSystem::Device::CONTROLLER;
+				new_device->index = i;
+				g_controllers.devices[i] = new_device;
+				g_controllers.input->addDevice(new_device);
+			}
+			else if (!g_controllers.connected[i] && g_controllers.devices[i])
+			{
+				g_controllers.input->removeDevice(g_controllers.devices[i]);
+				g_controllers.devices[i] = nullptr;
+			}
+		}
+	}
+	g_controllers.last_checked = (g_controllers .last_checked + 1) % XUSER_MAX_COUNT;
+}
+
+
+void ControllerDevice::shutdown()
+{
+	if (g_controllers.lib) FreeLibrary(g_controllers.lib);
+}
+
+
+} // namespace Lumix
