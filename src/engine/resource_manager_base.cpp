5dcb5f4c8362c67bf5b06f81388dcddd84db0d20
Mikulas Florek <mikulas.florek@gamedev.sk>
2017-10-23 18:52:39 +0200

    compile shaders on demand - closes #1180

--- src/engine/resource_manager_base.cpp
+++ src/engine/resource_manager_base.cpp
@@ -10,135 +10,165 @@
 
 namespace Lumix
 {
-	void ResourceManagerBase::create(ResourceType type, ResourceManager& owner)
-	{
-		owner.add(type, this);
-		m_owner = &owner;
-	}
 
-	void ResourceManagerBase::destroy(void)
+
+void ResourceManagerBase::LoadHook::continueLoad(Resource& resource)
+{
+	ASSERT(resource.isEmpty());
+	resource.m_desired_state = Resource::State::EMPTY;
+	resource.doLoad();
+}
+
+
+void ResourceManagerBase::create(ResourceType type, ResourceManager& owner)
+{
+	owner.add(type, this);
+	m_owner = &owner;
+}
+
+void ResourceManagerBase::destroy(void)
+{
+	for (auto iter = m_resources.begin(), end = m_resources.end(); iter != end; ++iter)
 	{
-		for (auto iter = m_resources.begin(), end = m_resources.end(); iter != end; ++iter)
+		Resource* resource = iter.value();
+		if (!resource->isEmpty())
 		{
-			Resource* resource = iter.value();
-			if (!resource->isEmpty())
-			{
-				g_log_error.log("Engine") << "Leaking resource " << resource->getPath().c_str() << "\n";
-			}
-			destroyResource(*resource);
+			g_log_error.log("Engine") << "Leaking resource " << resource->getPath().c_str() << "\n";
 		}
-		m_resources.clear();
+		destroyResource(*resource);
+	}
+	m_resources.clear();
+}
+
+Resource* ResourceManagerBase::get(const Path& path)
+{
+	ResourceTable::iterator it = m_resources.find(path.getHash());
+
+	if(m_resources.end() != it)
+	{
+		return *it;
 	}
 
-	Resource* ResourceManagerBase::get(const Path& path)
-	{
-		ResourceTable::iterator it = m_resources.find(path.getHash());
+	return nullptr;
+}
 
-		if(m_resources.end() != it)
+Resource* ResourceManagerBase::load(const Path& path)
+{
+	Resource* resource = get(path);
+
+	if(nullptr == resource)
+	{
+		resource = createResource(path);
+		m_resources.insert(path.getHash(), resource);
+	}
+
+	if(resource->isEmpty() && resource->m_desired_state == Resource::State::EMPTY)
+	{
+		if (m_load_hook && m_load_hook->onBeforeLoad(*resource))
 		{
-			return *it;
+			resource->m_desired_state = Resource::State::READY;
+			resource->addRef();
+			return resource;
 		}
-
-		return nullptr;
+		resource->doLoad();
 	}
 
-	Resource* ResourceManagerBase::load(const Path& path)
-	{
-		Resource* resource = get(path);
+	resource->addRef();
+	return resource;
+}
 
-		if(nullptr == resource)
+void ResourceManagerBase::removeUnreferenced()
+{
+	if (!m_is_unload_enabled) return;
+
+	Array<Resource*> to_remove(m_allocator);
+	for (auto* i : m_resources)
+	{
+		if (i->getRefCount() == 0) to_remove.push(i);
+	}
+
+	for (auto* i : to_remove)
+	{
+		m_resources.erase(i->getPath().getHash());
+		destroyResource(*i);
+	}
+}
+
+void ResourceManagerBase::load(Resource& resource)
+{
+	if(resource.isEmpty() && resource.m_desired_state == Resource::State::EMPTY)
+	{
+		if (m_load_hook && m_load_hook->onBeforeLoad(resource))
 		{
-			resource = createResource(path);
-			m_resources.insert(path.getHash(), resource);
+			resource.addRef();
+			return;
 		}
-		
-		if(resource->isEmpty())
-		{
-			resource->doLoad();
-		}
-
-		resource->addRef();
-		return resource;
-	}
-
-	void ResourceManagerBase::removeUnreferenced()
-	{
-		if (!m_is_unload_enabled) return;
-
-		Array<Resource*> to_remove(m_allocator);
-		for (auto* i : m_resources)
-		{
-			if (i->getRefCount() == 0) to_remove.push(i);
-		}
-
-		for (auto* i : to_remove)
-		{
-			m_resources.erase(i->getPath().getHash());
-			destroyResource(*i);
-		}
-	}
-
-	void ResourceManagerBase::load(Resource& resource)
-	{
-		if(resource.isEmpty())
-		{
-			resource.doLoad();
-		}
-
-		resource.addRef();
-	}
-
-	void ResourceManagerBase::unload(const Path& path)
-	{
-		Resource* resource = get(path);
-		if (resource) unload(*resource);
-	}
-
-	void ResourceManagerBase::unload(Resource& resource)
-	{
-		int new_ref_count = resource.remRef();
-		ASSERT(new_ref_count >= 0);
-		if(new_ref_count == 0 && m_is_unload_enabled)
-		{
-			resource.doUnload();
-		}
-	}
-
-	void ResourceManagerBase::reload(const Path& path)
-	{
-		Resource* resource = get(path);
-		if(resource) reload(*resource);
-	}
-
-	void ResourceManagerBase::reload(Resource& resource)
-	{
-		resource.doUnload();
 		resource.doLoad();
 	}
 
-	void ResourceManagerBase::enableUnload(bool enable)
+	resource.addRef();
+}
+
+void ResourceManagerBase::unload(const Path& path)
+{
+	Resource* resource = get(path);
+	if (resource) unload(*resource);
+}
+
+void ResourceManagerBase::unload(Resource& resource)
+{
+	int new_ref_count = resource.remRef();
+	ASSERT(new_ref_count >= 0);
+	if(new_ref_count == 0 && m_is_unload_enabled)
 	{
-		m_is_unload_enabled = enable;
-		if (!enable) return;
-
-		for (auto* resource : m_resources)
-		{
-			if (resource->getRefCount() == 0)
-			{
-				resource->doUnload();
-			}
-		}
-	}
-
-	ResourceManagerBase::ResourceManagerBase(IAllocator& allocator)
-		: m_resources(allocator)
-		, m_allocator(allocator)
-		, m_owner(nullptr)
-		, m_is_unload_enabled(true)
-	{ }
-
-	ResourceManagerBase::~ResourceManagerBase()
-	{
-		ASSERT(m_resources.empty());
+		resource.doUnload();
 	}
 }
+
+void ResourceManagerBase::reload(const Path& path)
+{
+	Resource* resource = get(path);
+	if(resource) reload(*resource);
+}
+
+void ResourceManagerBase::reload(Resource& resource)
+{
+	resource.doUnload();
+	resource.doLoad();
+}
+
+void ResourceManagerBase::enableUnload(bool enable)
+{
+	m_is_unload_enabled = enable;
+	if (!enable) return;
+
+	for (auto* resource : m_resources)
+	{
+		if (resource->getRefCount() == 0)
+		{
+			resource->doUnload();
+		}
+	}
+}
+
+void ResourceManagerBase::setLoadHook(LoadHook& load_hook)
+{
+	ASSERT(!m_load_hook);
+	m_load_hook = &load_hook;
+}
+
+ResourceManagerBase::ResourceManagerBase(IAllocator& allocator)
+	: m_resources(allocator)
+	, m_allocator(allocator)
+	, m_owner(nullptr)
+	, m_is_unload_enabled(true)
+	, m_load_hook(nullptr)
+{ }
+
+ResourceManagerBase::~ResourceManagerBase()
+{
+	ASSERT(m_resources.empty());
+}
+
+
+} // namespace Lumix
