b4760b1172831dd614dde34a14010d2d6d59a669
Mikulas Florek <mikulas.florek@gamedev.sk>
2017-11-06 15:25:23 +0100

    cleanup

--- src/editor/metadata.cpp
+++ src/editor/metadata.cpp
@@ -206,8 +206,9 @@ bool Metadata::setRawMemory(u32 file, u32 key, const void* mem, size_t size)
 	auto* data = getOrCreateData(file, key);
 	if (!data) return false;
 
+	if(data->m_type == DataItem::RAW_MEMORY) m_allocator.deallocate(data->m_raw.memory);
+
 	data->m_type = DataItem::RAW_MEMORY;
-	m_allocator.deallocate(data->m_raw.memory);
 	data->m_raw.memory = m_allocator.allocate(size);
 	copyMemory(data->m_raw.memory, mem, size);
 	data->m_raw.size = size;
@@ -221,6 +222,8 @@ bool Metadata::setInt(u32 file, u32 key, int value)
 	auto* data = getOrCreateData(file, key);
 	if (!data) return false;
 
+	if (data->m_type == DataItem::RAW_MEMORY) m_allocator.deallocate(data->m_raw.memory);
+
 	data->m_type = DataItem::INT;
 	data->m_int = value;
 
@@ -232,6 +235,8 @@ bool Metadata::setString(u32 file, u32 key, const char* value)
 {
 	auto* data = getOrCreateData(file, key);
 	if (!data) return false;
+
+	if (data->m_type == DataItem::RAW_MEMORY) m_allocator.deallocate(data->m_raw.memory);
 
 	data->m_type = DataItem::STRING;
 	copyString(data->m_string, value);
