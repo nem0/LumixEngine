e902e9236a0a289d2f1ab2efe65c9bec2dce381b
Mikulas Florek <mikulas.florek@gamedev.sk>
2018-01-08 13:08:11 +0100

    font manager WIP

--- src/renderer/font_manager.h
+++ src/renderer/font_manager.h
@@ -1,8 +1,10 @@
 #pragma once
 
 
+#include "engine/hash_map.h"
 #include "engine/resource.h"
 #include "engine/resource_manager_base.h"
+#include "renderer/draw2d.h"
 
 
 namespace Lumix
@@ -10,38 +12,52 @@ namespace Lumix
 
 
 class Renderer;
+class Texture;
 
 
 class FontResource LUMIX_FINAL : public Resource
 {
 public:
-	FontResource(const Path& path, ResourceManagerBase& manager, IAllocator& allocator)
-		: Resource(path, manager, allocator)
-	{
-	}
+	FontResource(const Path& path, ResourceManagerBase& manager, IAllocator& allocator);
 
 	ResourceType getType() const override { return ResourceType("font"); }
 
 	void unload() override {}
 	bool load(FS::IFile& file) override { return true; }
+	Font* addRef(int font_size);
+	void removeRef(Font& font);
+
+private:
+	struct FontRef
+	{
+		Font* font;
+		int ref_count;
+	};
+
+	HashMap<int, FontRef> m_fonts; 
 };
 
 
 class LUMIX_RENDERER_API FontManager LUMIX_FINAL : public ResourceManagerBase
 {
+friend class FontResource;
 public:
-	FontManager(IAllocator& allocator)
-		: ResourceManagerBase(allocator)
-		, m_allocator(allocator)
-	{}
-	~FontManager() {}
+	FontManager(Renderer& renderer, IAllocator& allocator);
+	FontAtlas& getFontAtlas() { return m_font_atlas; }
+	Font* getDefaultFont() const { return m_default_font; }
+	Texture* getAtlasTexture() const { return m_atlas_texture; }
 
-protected:
+private:
 	Resource* createResource(const Path& path) override;
 	void destroyResource(Resource& resource) override;
+	void updateFontTexture();
 
 private:
 	IAllocator& m_allocator;
+	Renderer& m_renderer;
+	FontAtlas m_font_atlas;
+	Font* m_default_font;
+	Texture* m_atlas_texture;
 };
 
 
