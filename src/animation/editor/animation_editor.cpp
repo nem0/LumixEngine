985872812c25a2bfc524cd6c4b54614926e49b80
Mikulas Florek <mikulas.florek@gamedev.sk>
2018-04-05 11:57:53 +0200

    fixed crash when editing anim graph's input without any entity selected

--- src/animation/editor/animation_editor.cpp
+++ src/animation/editor/animation_editor.cpp
@@ -143,9 +143,11 @@ struct InputValueCustomUI
 	template <typename Owner, typename PP, typename T>
 	static void build(Owner& owner, const PP& pp, T& value)
 	{
+
 		StudioApp& app = owner.resource.getEditor().getApp();
 
 		const auto& selected_entities = app.getWorldEditor().getSelectedEntities();
+		if (selected_entities.empty()) return;
 		auto* scene = (AnimationScene*)app.getWorldEditor().getUniverse()->getScene(ANIMABLE_HASH);
 		
 		if (!scene->getUniverse().hasComponent(selected_entities[0], CONTROLLER_TYPE)) return;
