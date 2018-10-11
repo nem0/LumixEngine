fd525a7996fa2a2aa936cc5188b2996569e20569
Mikulas Florek <mikulas.florek@gamedev.sk>
2018-10-11 22:01:56 +0200

    cleanup

--- src/engine/job_system.h
+++ src/engine/job_system.h
@@ -19,7 +19,6 @@ LUMIX_ENGINE_API bool init(IAllocator& allocator);
 LUMIX_ENGINE_API void shutdown();
 
 LUMIX_ENGINE_API CounterHandle run(void* data, void (*task)(void*));
-LUMIX_ENGINE_API CounterHandle runMany(void* data, void (*task)(void*), int count, int data_stride);
 LUMIX_ENGINE_API CounterHandle runAfter(void* data, void (*task)(void*), CounterHandle counter);
 LUMIX_ENGINE_API void wait(CounterHandle counter);
 LUMIX_ENGINE_API inline bool isValid(CounterHandle counter) { return counter != INVALID_HANDLE; }
