d90d42dc18ed181a3f73a76ee5210f3fc7cad74b
Mikulas Florek <mikulas.florek@gamedev.sk>
2019-10-09 22:01:54 +0200

    fixed dof, film grain and fxaa not working with multiple pipelines - closes #1293

--- data/pipelines/film_grain.lua
+++ data/pipelines/film_grain.lua
@@ -1,16 +1,14 @@
-local film_grain_shader = nil
-
 function postprocess(env, transparent_phase, ldr_buffer, gbuffer0, gbuffer1, gbuffer_depth, shadowmap)
 	if not enabled then return ldr_buffer end
 	if transparent_phase ~= "post_tonemap" then return ldr_buffer end
 	local res = env.createRenderbuffer(1, 1, true, "rgba8", "film_grain")
 	env.beginBlock("film_grain")
-	if film_grain_shader == nil then
-		film_grain_shader = env.preloadShader("pipelines/film_grain.shd")
+	if env.film_grain_shader == nil then
+		env.film_grain_shader = env.preloadShader("pipelines/film_grain.shd")
 	end
 
 	env.setRenderTargets(res)
-	env.drawArray(0, 4, film_grain_shader, 
+	env.drawArray(0, 4, env.film_grain_shader, 
 		{ ldr_buffer },
 		{},
 		{},
