56c5ac6b8b2ec47c699745de3babc27ee474c58f
Mikulas Florek <mikulas.florek@gamedev.sk>
2019-12-12 20:22:37 +0100

    cleanup

--- src/engine/lumix.h
+++ src/engine/lumix.h
@@ -4,19 +4,7 @@
 	#include <signal.h> // SIGTRAP
 #endif
 
-#ifdef _WIN32
-	#ifdef _WIN64
-		#define PLATFORM64
-	#else
-		#define PLATFORM32
-	#endif
-#elif defined(__linux__)
-	#ifdef __x86_64__
-		#define PLATFORM64
-	#else
-		#define PLATFORM32
-	#endif
-#else
+#ifndef _WIN32
 	#error Platform not supported
 #endif
 
@@ -53,20 +41,11 @@ typedef unsigned int u32;
 	typedef long long i64;
 	typedef unsigned long long u64;
 #else
-	#ifdef PLATFORM64
-		typedef long i64;
-		typedef unsigned long u64;
-	#else
-		typedef long long i64;
-		typedef unsigned long long u64;
-	#endif
+	typedef long i64;
+	typedef unsigned long u64;
 #endif
 
-#ifdef PLATFORM64
-	typedef u64 uintptr;
-#else
-	typedef u32 uintptr;
-#endif
+typedef u64 uintptr;
 
 static_assert(sizeof(uintptr) == sizeof(void*), "Incorrect size of uintptr");
 static_assert(sizeof(i64) == 8, "Incorrect size of i64");
