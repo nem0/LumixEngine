baa57556ebb36c44acba320495cb26ff6289eb61
Mikulas Florek <mikulas.florek@gamedev.sk>
2019-12-14 19:27:26 +0100

    cleanup

--- src/renderer/editor/ofbx.cpp
+++ src/renderer/editor/ofbx.cpp
@@ -176,13 +176,13 @@ static Matrix getRotationMatrix(const Vec3& euler, RotationOrder order)
 	switch (order)
 	{
 		default:
-		case RotationOrder::SPHERIC_XYZ: assert(false);
 		case RotationOrder::EULER_XYZ: return rz * ry * rx;
 		case RotationOrder::EULER_XZY: return ry * rz * rx;
 		case RotationOrder::EULER_YXZ: return rz * rx * ry;
 		case RotationOrder::EULER_YZX: return rx * rz * ry;
 		case RotationOrder::EULER_ZXY: return ry * rx * rz;
 		case RotationOrder::EULER_ZYX: return rx * ry * rz;
+		case RotationOrder::SPHERIC_XYZ: assert(false); Error::s_message = "Unsupported rotation order."; return rx * ry * rz;
 	}
 }
 
