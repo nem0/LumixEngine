9b6c50e459f8a9fc1dcf48b3063d444326d164e4
Mikulas Florek <mikulas.florek@gamedev.sk>
2020-04-09 01:00:08 +0200

    film grain uses texture as a source of noise

--- data/pipelines/film_grain.lua
+++ data/pipelines/film_grain.lua
@@ -1,6 +1,10 @@
+noise = -1
+Editor.setPropertyType(this, "noise", Editor.RESOURCE_PROPERTY, "texture")
+
 function postprocess(env, transparent_phase, ldr_buffer, gbuffer0, gbuffer1, gbuffer_depth, shadowmap)
 	if not enabled then return ldr_buffer end
 	if transparent_phase ~= "post_tonemap" then return ldr_buffer end
+	if noise == -1 then return ldr_buffer end
 	local res = env.createRenderbuffer(1, 1, true, "rgba8", "film_grain")
 	env.beginBlock("film_grain")
 	if env.film_grain_shader == nil then
@@ -8,6 +12,7 @@ function postprocess(env, transparent_phase, ldr_buffer, gbuffer0, gbuffer1, gbu
 	end
 
 	env.setRenderTargets(res)
+	env.bindTextures({noise}, 1)
 	env.drawArray(0, 4, env.film_grain_shader, 
 		{ ldr_buffer },
 		{},
