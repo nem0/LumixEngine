f5bb2fa0a8a999f535a4814c3ad7939d1e1c3789
Mikulas Florek <mikulas.florek@gamedev.sk>
2020-09-03 23:49:39 +0200

    atmo WIP

--- data/pipelines/atmo.lua
+++ data/pipelines/atmo.lua
@@ -8,8 +8,8 @@ function postprocess(env, transparent_phase, hdr_buffer, gbuffer0, gbuffer1, gbu
 		env.atmo_shader = env.preloadShader("pipelines/atmo.shd")
 		env.atmo_scattering_shader = env.preloadShader("pipelines/atmo_scattering.shd")
 		env.atmo_transmittance_shader = env.preloadShader("pipelines/atmo_transmittance.shd")
-		env.inscatter_precomputed = env.createTexture3D(64, 128, 32, "rgba32f")
-		env.transmittance_precomputed = env.createTexture2D(128, 128, "rgba32f")
+		env.inscatter_precomputed = env.createTexture2D(64, 128, "rgba32f")
+		env.transmittance_precomputed = env.createTexture2D(128, 128, "rg32f")
 		
 	end
 	env.setRenderTargetsReadonlyDS(hdr_buffer, gbuffer_depth)
@@ -28,12 +28,15 @@ function postprocess(env, transparent_phase, hdr_buffer, gbuffer0, gbuffer1, gbu
 		state.stencil_zpass = env.STENCIL_KEEP
 	end
 	
-	env.beginBlock("precompute_atmo")
+	env.beginBlock("precompute_transmittance")
 	env.bindImageTexture(env.transmittance_precomputed, 0)
 	env.dispatch(env.atmo_transmittance_shader, 128 / 16, 128 / 16, 1)
 	env.bindImageTexture(env.inscatter_precomputed, 0)
 	env.bindRawTexture(env.transmittance_precomputed, 1)
-	env.dispatch(env.atmo_scattering_shader, 64 / 16, 128 / 16, 32)
+	env.endBlock()
+
+	env.beginBlock("precompute_inscatter")
+	env.dispatch(env.atmo_scattering_shader, 64 / 16, 128 / 16, 1)
 	env.endBlock()
 
 	env.bindRawTexture(env.inscatter_precomputed, 1);
