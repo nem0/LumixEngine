9192de322d1eb5d639a0bcb1dc87062eecf032dd
Mikulas Florek <mikulas.florek@gamedev.sk>
2021-01-21 21:16:09 +0100

    synced openfbx

--- src/renderer/editor/ofbx.cpp
+++ src/renderer/editor/ofbx.cpp
@@ -3144,10 +3144,12 @@ static bool parseObjects(const Element& root, Scene* scene, u64 flags, Allocator
 		}
 	}
 
-	(*job_processor)([](void* ptr){
-		ParseGeometryJob* job = (ParseGeometryJob*)ptr;
-		job->is_error = parseGeometry(*job->element, job->triangulate, job->geom).isError();
-	}, job_user_ptr, &parse_geom_jobs[0], (u32)sizeof(parse_geom_jobs[0]), (u32)parse_geom_jobs.size());
+	if (!parse_geom_jobs.empty()) {
+		(*job_processor)([](void* ptr){
+			ParseGeometryJob* job = (ParseGeometryJob*)ptr;
+			job->is_error = parseGeometry(*job->element, job->triangulate, job->geom).isError();
+		}, job_user_ptr, &parse_geom_jobs[0], (u32)sizeof(parse_geom_jobs[0]), (u32)parse_geom_jobs.size());
+	}
 
 	for (const ParseGeometryJob& job : parse_geom_jobs) {
 		if (job.is_error) return false;
