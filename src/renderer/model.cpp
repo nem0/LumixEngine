483f45716684eba0b5c67bf203c3902083e42498
Mikulas Florek <mikulas.florek@gamedev.sk>
2021-04-11 17:45:34 +0200

    vertex ao shader

--- src/renderer/model.cpp
+++ src/renderer/model.cpp
@@ -283,8 +283,8 @@ static u8 getIndexBySemantic(Mesh::AttributeSemantic semantic) {
 		case Mesh::AttributeSemantic::TANGENT: return 3;
 		case Mesh::AttributeSemantic::INDICES: return 4;
 		case Mesh::AttributeSemantic::WEIGHTS: return 5;
-		case Mesh::AttributeSemantic::AO: return 6;
 		case Mesh::AttributeSemantic::COLOR0: return 7;
+		case Mesh::AttributeSemantic::AO: return 8;
 	}
 	ASSERT(false);
 	return 0;
