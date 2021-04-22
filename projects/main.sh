f05d23f787881f161cb1a8969aff40dec5a3c8e3
Mikulas Florek <mikulas.florek@gamedev.sk>
2021-04-22 23:49:00 +0200

    cleanup

--- projects/main.sh
+++ projects/main.sh
@@ -29,15 +29,6 @@ build_luajit()
     popd
 }
 
-build_nvtt()
-{
-    pushd 3rdparty/nvtt
-    chmod +x configure
-    ./configure --release
-    make
-    popd
-}
-
 build_physx()
 {
     pushd 3rdparty/physx/physx
@@ -64,7 +55,6 @@ build_3rdparty()
     case "$1" in
         "freetype" ) build_freetype;;
         "luajit" ) build_luajit;;
-        "nvtt" ) build_nvtt;;
         "physx" ) build_physx;;
         "recast" ) build_recast;;
     esac
@@ -80,19 +70,6 @@ deploy_luajit()
 {
     mkdir -p ../external/luajit/lib/linux64_gmake/release
     cp 3rdparty/luajit/src/libluajit.a ../external/luajit/lib/linux64_gmake/release/libluajit.a
-}
-
-deploy_nvtt()
-{
-    mkdir -p ../external/nvtt/lib/linux64_gmake/release
-    cp 3rdparty/nvtt/build-release/src/nvtt/libnvtt.a ../external/nvtt/lib/linux64_gmake/release/
-    cp 3rdparty/nvtt/build-release/src/nvtt/squish/libsquish.a ../external/nvtt/lib/linux64_gmake/release/
-    cp 3rdparty/nvtt/build-release/src/bc6h/libbc6h.a ../external/nvtt/lib/linux64_gmake/release/
-    cp 3rdparty/nvtt/build-release/src/bc7/libbc7.a ../external/nvtt/lib/linux64_gmake/release/
-    cp 3rdparty/nvtt/build-release/src/nvimage/libnvimage.a ../external/nvtt/lib/linux64_gmake/release/
-    cp 3rdparty/nvtt/build-release/src/nvcore/libnvcore.a ../external/nvtt/lib/linux64_gmake/release/
-    cp 3rdparty/nvtt/build-release/src/nvmath/libnvmath.a ../external/nvtt/lib/linux64_gmake/release/
-    cp 3rdparty/nvtt/build-release/src/nvthread/libnvthread.a ../external/nvtt/lib/linux64_gmake/release/
 }
 
 deploy_recast()
@@ -112,7 +89,6 @@ deploy_3rdparty()
     case "$1" in
         "freetype" ) deploy_freetype;;
         "luajit" ) deploy_luajit;;
-        "nvtt" ) deploy_nvtt;;
         "physx" ) deploy_physx;;
         "recast" ) deploy_recast;;
     esac
@@ -140,19 +116,16 @@ init_3rdparty()
 {
     download_project "freetype" "https://github.com/nem0/freetype2.git"
     download_project "luajit" "https://github.com/nem0/LuaJIT.git"
-    download_project "nvtt" "https://github.com/nem0/nvidia-texture-tools.git"
     download_project "physx" "https://github.com/nem0/PhysX.git"
     download_project "recast" "https://github.com/nem0/recastnavigation.git"
 
     build_3rdparty "freetype"
     build_3rdparty "luajit"
-    build_3rdparty "nvtt"
     build_3rdparty "physx"
     build_3rdparty "recast"
 
     deploy_3rdparty "freetype"
     deploy_3rdparty "luajit"
-    deploy_3rdparty "nvtt"
     deploy_3rdparty "physx"
     deploy_3rdparty "recast"
 
@@ -164,17 +137,16 @@ thirdparty_menu()
     while :; do
         clear;
         echo "third party"
-        options=("freetype" "luajit" "nvtt" "physx" "recast" "init all" "back")
+        options=("freetype" "luajit" "physx" "recast" "init all" "back")
         select opt in "${options[@]}"
         do
             case "$REPLY" in
                 1 ) project_menu "freetype" "https://github.com/nem0/freetype2.git"; break;;
                 2 ) project_menu "luajit" "https://github.com/nem0/LuaJIT.git"; break;;
-                3 ) project_menu "nvtt" "https://github.com/nem0/nvidia-texture-tools.git"; break;;
-                4 ) project_menu "physx" "https://github.com/nem0/PhysX.git"; break;;
-                5 ) project_menu "recast" "https://github.com/nem0/recastnavigation.git"; break;;
-                6 ) init_3rdparty; break;;
-                7 ) break 2;;
+                3 ) project_menu "physx" "https://github.com/nem0/PhysX.git"; break;;
+                4 ) project_menu "recast" "https://github.com/nem0/recastnavigation.git"; break;;
+                5 ) init_3rdparty; break;;
+                6 ) break 2;;
             esac
         done
     done
