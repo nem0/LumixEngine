b0dc25594fb7c2c1c7a534332d9722cb2930798b
Mikulas Florek <mikulas.florek@gamedev.sk>
2021-05-04 19:36:20 +0200

    fixed wrong changed: cannot open `changed' (No such file or directory) notifications

--- src/editor/win/file_system_watcher.cpp
+++ src/editor/win/file_system_watcher.cpp
@@ -1,38 +1,29 @@
 #include "engine/allocator.h"
 #include "engine/crt.h"
 #include "engine/delegate.h"
-#include "engine/thread.h"
 #include "engine/profiler.h"
 #include "engine/string.h"
-#include "file_system_watcher.h"
+#include "engine/thread.h"
 
 #include "engine/win/simple_win.h"
+#include "file_system_watcher.h"
 
 
-namespace Lumix
-{
+namespace Lumix {
 
 
 struct FileSystemWatcherPC;
 
 
-static const DWORD READ_DIR_CHANGE_FILTER =
-	FILE_NOTIFY_CHANGE_SECURITY | FILE_NOTIFY_CHANGE_CREATION | FILE_NOTIFY_CHANGE_LAST_ACCESS |
-	FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_ATTRIBUTES |
-	FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_FILE_NAME;
+static const DWORD READ_DIR_CHANGE_FILTER = FILE_NOTIFY_CHANGE_CREATION | FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_FILE_NAME;
 
-
-struct FileSystemWatcherTask final : Thread
-{
-	FileSystemWatcherTask(const char* path,
-		FileSystemWatcherPC& watcher,
-		IAllocator& allocator)
+struct FileSystemWatcherTask final : Thread {
+	FileSystemWatcherTask(const char* path, FileSystemWatcherPC& watcher, IAllocator& allocator)
 		: Thread(allocator)
 		, m_watcher(watcher)
 	{
-		copyString(m_path, path);
+		m_path = path;
 	}
-
 
 	int task() override;
 
@@ -41,24 +32,20 @@ struct FileSystemWatcherTask final : Thread
 	HANDLE m_handle;
 	DWORD m_received;
 	OVERLAPPED m_overlapped;
-	char m_path[LUMIX_MAX_PATH];
+	StaticString<LUMIX_MAX_PATH> m_path;
 	FileSystemWatcherPC& m_watcher;
 };
 
 
-struct FileSystemWatcherPC final : FileSystemWatcher
-{
+struct FileSystemWatcherPC final : FileSystemWatcher {
 	explicit FileSystemWatcherPC(IAllocator& allocator)
-		: m_allocator(allocator)
-	{
+		: m_allocator(allocator) {
 		m_task = nullptr;
 	}
 
 
-	virtual ~FileSystemWatcherPC()
-	{
-		if (m_task)
-		{
+	~FileSystemWatcherPC() override {
+		if (m_task) {
 			CancelIoEx(m_task->m_handle, nullptr);
 			CloseHandle(m_task->m_handle);
 
@@ -68,11 +55,9 @@ struct FileSystemWatcherPC final : FileSystemWatcher
 	}
 
 
-	bool start(LPCSTR path)
-	{
+	bool start(LPCSTR path) {
 		m_task = LUMIX_NEW(m_allocator, FileSystemWatcherTask)(path, *this, m_allocator);
-		if (!m_task->create("Filesystem watcher", true))
-		{
+		if (!m_task->create("Filesystem watcher", true)) {
 			LUMIX_DELETE(m_allocator, m_task);
 			m_task = nullptr;
 			return false;
@@ -90,53 +75,43 @@ struct FileSystemWatcherPC final : FileSystemWatcher
 };
 
 
-UniquePtr<FileSystemWatcher> FileSystemWatcher::create(const char* path, IAllocator& allocator)
-{
+UniquePtr<FileSystemWatcher> FileSystemWatcher::create(const char* path, IAllocator& allocator) {
 	UniquePtr<FileSystemWatcherPC> watcher = UniquePtr<FileSystemWatcherPC>::create(allocator, allocator);
-	if (!watcher->start(path))
-	{
+	if (!watcher->start(path)) {
 		return UniquePtr<FileSystemWatcher>();
 	}
 	return watcher.move();
 }
 
 
-static void wcharToCharArray(const WCHAR* src, char* dest, int len)
-{
-	for (unsigned int i = 0; i < len / sizeof(WCHAR); ++i)
-	{
+static void wcharToCharArray(const WCHAR* src, char* dest, int len) {
+	for (unsigned int i = 0; i < len / sizeof(WCHAR); ++i) {
 		dest[i] = static_cast<char>(src[i]);
 	}
 	dest[len / sizeof(WCHAR)] = '\0';
 }
 
 
-static void CALLBACK notif(DWORD status, DWORD tferred, LPOVERLAPPED over)
-{
+static void CALLBACK notif(DWORD status, DWORD tferred, LPOVERLAPPED over) {
 	auto* task = (FileSystemWatcherTask*)over->hEvent;
-	if (status == ERROR_OPERATION_ABORTED)
-	{
+	if (status == ERROR_OPERATION_ABORTED) {
 		task->m_finished = true;
 		return;
 	}
 	if (tferred == 0) return;
 
 	FILE_NOTIFY_INFORMATION* info = (FILE_NOTIFY_INFORMATION*)task->m_info;
-	while (info)
-	{
+	while (info) {
 		auto action = info->Action;
-		switch (action)
-		{
+		switch (action) {
 			case FILE_ACTION_RENAMED_NEW_NAME:
 			case FILE_ACTION_ADDED:
 			case FILE_ACTION_MODIFIED:
-			case FILE_ACTION_REMOVED:
-			{
+			case FILE_ACTION_REMOVED: {
 				char tmp[MAX_PATH];
 				wcharToCharArray(info->FileName, tmp, info->FileNameLength);
 				task->m_watcher.m_callback.invoke(tmp);
-			}
-			break;
+			} break;
 			case FILE_ACTION_RENAMED_OLD_NAME:
 				char tmp[MAX_PATH];
 				wcharToCharArray(info->FileName, tmp, info->FileNameLength);
@@ -144,38 +119,21 @@ static void CALLBACK notif(DWORD status, DWORD tferred, LPOVERLAPPED over)
 				break;
 			default: ASSERT(false); break;
 		}
-		info = info->NextEntryOffset == 0
-				   ? nullptr
-				   : (FILE_NOTIFY_INFORMATION*)(((u8*)info) + info->NextEntryOffset);
+		info = info->NextEntryOffset == 0 ? nullptr : (FILE_NOTIFY_INFORMATION*)(((u8*)info) + info->NextEntryOffset);
 	}
 }
 
 
-int FileSystemWatcherTask::task()
-{
-	m_handle = CreateFile(m_path,
-		FILE_LIST_DIRECTORY,
-		FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
-		nullptr,
-		OPEN_EXISTING,
-		FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED,
-		nullptr);
+int FileSystemWatcherTask::task() {
+	m_handle = CreateFile(m_path, FILE_LIST_DIRECTORY, FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED, nullptr);
 	if (m_handle == INVALID_HANDLE_VALUE) return -1;
 
 	memset(&m_overlapped, 0, sizeof(m_overlapped));
 	m_overlapped.hEvent = this;
 	m_finished = false;
-	while (!m_finished)
-	{
+	while (!m_finished) {
 		PROFILE_BLOCK("change handling");
-		BOOL status = ReadDirectoryChangesW(m_handle,
-			m_info,
-			sizeof(m_info),
-			TRUE,
-			READ_DIR_CHANGE_FILTER,
-			&m_received,
-			&m_overlapped,
-			&notif);
+		BOOL status = ReadDirectoryChangesW(m_handle, m_info, sizeof(m_info), TRUE, READ_DIR_CHANGE_FILTER, &m_received, &m_overlapped, &notif);
 		if (status == FALSE) break;
 		SleepEx(INFINITE, TRUE);
 	}
