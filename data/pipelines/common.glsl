48631085e6526bb4ce5ae9436927408d65bd419c
Mikulas Florek <mikulas.florek@gamedev.sk>
2022-04-03 17:06:40 +0200

    impostor in deferred rendering

--- data/pipelines/common.glsl
+++ data/pipelines/common.glsl
@@ -586,7 +586,7 @@ float rand(vec3 seed)
 void packSurface(Surface surface, out vec4 gbuffer0, out vec4 gbuffer1, out vec4 gbuffer2) {
 	gbuffer0 = vec4(surface.albedo.rgb, surface.roughness);
 	gbuffer1 = vec4(surface.N * 0.5 + 0.5, surface.metallic);
-	gbuffer2 = vec4(packEmission(surface.emission), surface.translucency, surface.ao, 1);
+	gbuffer2 = vec4(packEmission(surface.emission), surface.translucency, surface.ao, surface.shadow);
 }
 
 Surface unpackSurface(vec2 uv, sampler2D gbuffer0, sampler2D gbuffer1, sampler2D gbuffer2, sampler2D gbuffer_depth, out float ndc_depth) {
