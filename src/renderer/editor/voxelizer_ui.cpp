e9476318aa8845d3bfd7b46a267afc27ef84e786
Mikulas Florek <mikulas.florek@gamedev.sk>
2022-12-04 11:38:24 +0100

    voxelizer - fixed multimesh models

--- src/renderer/editor/voxelizer_ui.cpp
+++ src/renderer/editor/voxelizer_ui.cpp
@@ -55,11 +55,11 @@ void forEachTriangle(const Mesh& mesh, const F& f) {
 void VoxelizerUI::voxelize() {
 	PROFILE_FUNCTION();
 	ASSERT(m_model && m_model->isReady());
-	
+
+	Vec3 min = Vec3(FLT_MAX);
+	Vec3 max = Vec3(-FLT_MAX);
 	for (u32 mesh_idx = 0; mesh_idx < (u32)m_model->getMeshCount(); ++mesh_idx) {
 		const Mesh& mesh = m_model->getMesh(mesh_idx);
-		Vec3 min = Vec3(FLT_MAX);
-		Vec3 max = Vec3(-FLT_MAX);
 		forEachTriangle(mesh, [&](const Vec3& p0, const Vec3& p1, const Vec3& p2){
 			min = minimum(min, p0);
 			min = minimum(min, p1);
@@ -69,38 +69,40 @@ void VoxelizerUI::voxelize() {
 			max = maximum(max, p1);
 			max = maximum(max, p2);
 		});
+	}
 
-		const float voxel_size = maximum(max.x - min.x, max.y - min.y, max.z - min.z) / m_max_resolution;
-		min -= Vec3(voxel_size * 1.5f);
-		max += Vec3(voxel_size * 1.5f);
+	const float voxel_size = maximum(max.x - min.x, max.y - min.y, max.z - min.z) / m_max_resolution;
+	min -= Vec3(voxel_size * 1.5f);
+	max += Vec3(voxel_size * 1.5f);
+	const IVec3 resolution = IVec3(i32((max.x - min.x) / voxel_size), i32((max.y - min.y) / voxel_size), i32((max.z - min.z) / voxel_size));
+	m_voxels.resize(resolution.x * resolution.y * resolution.z);
+	memset(m_voxels.getMutableData(), 0, m_voxels.size());
 
-		const IVec3 resolution = IVec3(i32((max.x - min.x) / voxel_size), i32((max.y - min.y) / voxel_size), i32((max.z - min.z) / voxel_size));
-		m_voxels.resize(resolution.x * resolution.y * resolution.z);
-		memset(m_voxels.getMutableData(), 0, m_voxels.size());
+	auto to_grid = [&](const Vec3& p){
+		return IVec3((p - min) / voxel_size + Vec3(0.5f));
+	};
 
-		auto to_grid = [&](const Vec3& p){
-			return IVec3((p - min) / voxel_size + Vec3(0.5f));
-		};
+	auto from_grid = [&](const IVec3& p){
+		return Vec3(p) * voxel_size + Vec3(0.5f * voxel_size) + min;
+	};
 
-		auto from_grid = [&](const IVec3& p){
-			return Vec3(p) * voxel_size + Vec3(0.5f * voxel_size) + min;
-		};
+	auto intersect = [&](const Vec3& p0, const Vec3& p1, const Vec3& p2, IVec3 voxel){
+		Vec3 center = from_grid(voxel);
+		Vec3 half(0.5f * voxel_size);
+		return testAABBTriangleCollision(AABB(center - half, center + half), p0, p1, p2);
+	};
 
-		auto intersect = [&](const Vec3& p0, const Vec3& p1, const Vec3& p2, IVec3 voxel){
-			Vec3 center = from_grid(voxel);
-			Vec3 half(0.5f * voxel_size);
-			return testAABBTriangleCollision(AABB(center - half, center + half), p0, p1, p2);
-		};
+	auto setVoxel = [&](i32 i, i32 j, i32 k, u8 value){
+		if (i < 0 || j < 0 || k < 0) return;
+		if (i >= resolution.x) return;
+		if (j >= resolution.y) return;
+		if (k >= resolution.z) return;
 
-		auto setVoxel = [&](i32 i, i32 j, i32 k, u8 value){
-			if (i < 0 || j < 0 || k < 0) return;
-			if (i >= resolution.x) return;
-			if (j >= resolution.y) return;
-			if (k >= resolution.z) return;
+		m_voxels[i + j * (resolution.x) + k * (resolution.x * resolution.y)] = value;
+	};
 
-			m_voxels[i + j * (resolution.x) + k * (resolution.x * resolution.y)] = value;
-		};
-
+	for (u32 mesh_idx = 0; mesh_idx < (u32)m_model->getMeshCount(); ++mesh_idx) {
+		const Mesh& mesh = m_model->getMesh(mesh_idx);
 		forEachTriangle(mesh, [&](const Vec3& p0, const Vec3& p1, const Vec3& p2){
 			AABB aabb;
 			aabb.min = aabb.max = p0;
@@ -120,49 +122,50 @@ void VoxelizerUI::voxelize() {
 				}
 			}
 		});
-		m_grid_resolution = resolution;
-		m_voxel_size = voxel_size;
+	}
 
-		{
-			m_debug_triangles.clear();
-			Vec3 x(m_voxel_size * 0.5f, 0, 0);
-			Vec3 y(0, m_voxel_size * 0.5f, 0);
-			Vec3 z(0, 0, m_voxel_size * 0.5f);
-			Vec3 origin_shift = x * (float)m_grid_resolution.x + y * (float)m_grid_resolution.y + z * (float)m_grid_resolution.z;
-			for (u32 v = 0, c = (u32)m_voxels.size(); v < c; ++v) {
-				if (!m_voxels[v]) continue;
+	m_grid_resolution = resolution;
+	m_voxel_size = voxel_size;
+
+	{
+		m_debug_triangles.clear();
+		Vec3 x(m_voxel_size * 0.5f, 0, 0);
+		Vec3 y(0, m_voxel_size * 0.5f, 0);
+		Vec3 z(0, 0, m_voxel_size * 0.5f);
+		Vec3 origin_shift = x * (float)m_grid_resolution.x + y * (float)m_grid_resolution.y + z * (float)m_grid_resolution.z;
+		for (u32 v = 0, c = (u32)m_voxels.size(); v < c; ++v) {
+			if (!m_voxels[v]) continue;
 		
-				const i32 k = v / (m_grid_resolution.x * m_grid_resolution.y);
-				const i32 j = (v / m_grid_resolution.x) % m_grid_resolution.y;
-				const i32 i = v % m_grid_resolution.x;
-				const Vec3 from = Vec3((float)i * x * 2 + (float)j * y * 2 + (float)k * z * 2) - origin_shift;
+			const i32 k = v / (m_grid_resolution.x * m_grid_resolution.y);
+			const i32 j = (v / m_grid_resolution.x) % m_grid_resolution.y;
+			const i32 i = v % m_grid_resolution.x;
+			const Vec3 from = Vec3((float)i * x * 2 + (float)j * y * 2 + (float)k * z * 2) - origin_shift;
 				
-				const Vec3 points[] = {
-					from + x + y + z,
-					from + x + y - z,
-					from + x - y + z,
-					from + x - y - z,
+			const Vec3 points[] = {
+				from + x + y + z,
+				from + x + y - z,
+				from + x - y + z,
+				from + x - y - z,
 
-					from - x + y + z,
-					from - x + y - z,
-					from - x - y + z,
-					from - x - y - z,
-				};
+				from - x + y + z,
+				from - x + y - z,
+				from - x - y + z,
+				from - x - y - z,
+			};
 
-				const u32 idcs[] = { 
-					0, 1, 2,   1, 2, 3, // +x
-					4, 5, 6,   5, 6, 7, // -x
+			const u32 idcs[] = { 
+				0, 1, 2,   1, 2, 3, // +x
+				4, 5, 6,   5, 6, 7, // -x
 
-					0, 1, 4,   1, 4, 5, // +y
-					2, 3, 6,   3, 6, 7, // -y
+				0, 1, 4,   1, 4, 5, // +y
+				2, 3, 6,   3, 6, 7, // -y
 
-					0, 2, 4,   2, 4, 6, // +z
-					1, 3, 5,   3, 5, 7, // -z
-				};
+				0, 2, 4,   2, 4, 6, // +z
+				1, 3, 5,   3, 5, 7, // -z
+			};
 
-				for (u32 idx : idcs) {
-					m_debug_triangles.push(points[idx]);
-				}
+			for (u32 idx : idcs) {
+				m_debug_triangles.push(points[idx]);
 			}
 		}
 	}
