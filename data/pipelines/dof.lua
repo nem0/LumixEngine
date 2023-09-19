cd0b421cd5b235875d42e01b1dd2c4dff75e2bc2
Mikulas Florek <mikulas.florek@gamedev.sk>
2023-09-19 18:56:19 +0200

    new dof

--- data/pipelines/dof.lua
+++ data/pipelines/dof.lua
@@ -1,74 +1,29 @@
 return {
-	near_blur = 1,
-	near_sharp = 3,
-	far_sharp = 20,
-	far_blur = 30,
+	focus_distance = 10,
+	blur_range = 20,
 	enabled = false,
+	max_blur_size = 10,
+	sharp_range = 0,
 
 	gui = function(self)
 		_, self.enabled = ImGui.Checkbox("Enabled", self.enabled)
-		_, self.near_blur = ImGui.DragFloat("Near blur", self.near_blur)
-		_, self.near_sharp = ImGui.DragFloat("Near sharp", self.near_sharp)
-		_, self.far_sharp = ImGui.DragFloat("Far sharp", self.far_sharp)
-		_, self.far_blur = ImGui.DragFloat("Far blur", self.far_blur)
+		_, self.focus_distance = ImGui.DragFloat("Focus distance", self.focus_distance)
+		_, self.sharp_range = ImGui.DragFloat("Sharp range", self.sharp_range)
+		_, self.blur_range = ImGui.DragFloat("Blur range", self.blur_range)
+		_, self.max_blur_size = ImGui.DragFloat("Max blur size", self.max_blur_size)
 	end,
 
 	postprocess = function(self, env, input, gbuffer, shadowmap)
 		if not self.enabled then return input end
-		env.beginBlock("dof")
-		if env.dof_shader == nil then
-			env.dof_shader = env.preloadShader("pipelines/dof.shd")
-			env.dof_blur_shader = env.preloadShader("pipelines/dof_blur.shd")
-		end
 
+		env.beginBlock("dof")
+		env.dof_shader = env.dof_shader or env.preloadShader("pipelines/dof.shd")
 		env.dof_rb_desc = env.dof_rb_desc or env.createRenderbufferDesc { format = "rgba16f", debug_name = "dof_tmp" }
 		local tmp_rb = env.createRenderbuffer(env.dof_rb_desc)
 		
-		env.dof_state = env.dof_state or env.createRenderState({ depth_test = false, blending = "alpha"})
+		env.drawcallUniforms(self.focus_distance, self.blur_range, self.max_blur_size, self.sharp_range)
 
-		env.setRenderTargets(tmp_rb)
-		env.drawcallUniforms(self.near_blur, self.near_sharp, self.far_sharp, self.far_blur)
-
-		env.drawArray(0, 3, env.dof_blur_shader, 
-			{ 
-				input,
-				gbuffer.DS
-			},
-			env.dof_state,
-			{ "HBLUR", "NEAR" }
-		)
-
-		env.setRenderTargets(input)
-		env.drawArray(0, 3, env.dof_blur_shader, 
-			{
-				tmp_rb,
-				gbuffer.DS
-			},
-			env.dof_state,
-			{ "NEAR" }
-		)
-
-		env.setRenderTargets(tmp_rb)
-		env.drawArray(0, 3, env.dof_blur_shader, 
-			{ 
-				input,
-				gbuffer.DS
-			},
-			env.dof_state,
-			{ "HBLUR", "FAR" }
-		)
-
-		env.setRenderTargets(input)
-		env.drawArray(0, 3, env.dof_blur_shader, 
-			{
-				tmp_rb,
-				gbuffer.DS
-			},
-			env.dof_state,
-			{ "FAR" }
-		)
-
-
+		-- TODO optimize - halfres
 		env.setRenderTargets(tmp_rb)
 		env.drawArray(0, 3, env.dof_shader, 
 			{
@@ -77,8 +32,22 @@ return {
 			}
 		)
 
-		
+		env.drawcallUniforms( 
+			0, 0, 1, 1, 
+			1, 0, 0, 0, 
+			0, 1, 0, 0, 
+			0, 0, 1, 0, 
+			0, 0, 0, 1, 
+			0, 0, 0, 1
+		)
+	
+		env.setRenderTargets(input)
+		env.drawArray(0, 3, env.textured_quad_shader
+			, { tmp_rb }
+			, env.empty_state
+		)
+
 		env.endBlock()
-		return tmp_rb
+		return input
 	end
 }
