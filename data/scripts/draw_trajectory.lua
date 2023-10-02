3a1706d77cc94b12c150d9de511c17fcface7bc0
Mikulas Florek <mikulas.florek@gamedev.sk>
2023-10-02 22:45:16 +0200

    draw trajectory script

---
+++ data/scripts/draw_trajectory.lua
@@ -0,0 +1,10 @@
+local points = {}
+
+function update(time_delta)
+	if #points < 100 then
+		table.insert(points, this.position)
+	end
+	for i = 2, #points do
+		this.world.renderer:addDebugLine(points[i - 1], points[i], {1, 0, 0})
+	end
+end
