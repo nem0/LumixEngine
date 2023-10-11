4303389c5d5d6bd73372068eb2a7e9ceca284a68
Mikulas Florek <mikulas.florek@gamedev.sk>
2023-10-11 18:47:37 +0200

    fixed resource input height

--- src/editor/asset_browser.cpp
+++ src/editor/asset_browser.cpp
@@ -1183,7 +1183,9 @@ struct AssetBrowserImpl : AssetBrowser {
 		static TextFilter filter;
 		filter.gui("Filter", width, focus);
 		
-		ImGui::BeginChild("Resources", ImVec2(0, 200), false, ImGuiWindowFlags_HorizontalScrollbar);
+		float h = ImGui::GetContentRegionAvail().y - ImGui::GetTextLineHeightWithSpacing() * 2;
+
+		ImGui::BeginChild("Resources", ImVec2(0, h), false, ImGuiWindowFlags_HorizontalScrollbar);
 		AssetCompiler& compiler = m_app.getAssetCompiler();
 	
 		const auto& resources = compiler.lockResources();
