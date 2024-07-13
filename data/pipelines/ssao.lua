5b7bd6ae010ba46c47d3ce82ebb371a0b7ea147a
Mikulas Florek <mikulas.florek@gamedev.sk>
2024-07-13 21:36:54 +0200

    SSAO improvements

--- data/pipelines/ssao.lua
+++ data/pipelines/ssao.lua
@@ -6,24 +6,39 @@ return {
 	current_frame_weight = 0.05,
 	history_buf = -1,
 	enabled = true,
+	scale = 0.5,
+	discocclusion_strength = 20,
+	scale_changed = false,
 
 	gui = function(self)
 		_, self.enabled = ImGui.Checkbox("Enabled", self.enabled)
 		_, self.intensity = ImGui.DragFloat("Intensity", self.intensity)
 		_, self.radius = ImGui.DragFloat("Radius", self.radius)
+		self.scale_changed, self.scale = ImGui.DragFloat("Scale", self.scale)
+		_, self.discocclusion_strength = ImGui.DragFloat("Disocclusion", self.discocclusion_strength)
 		_, self.use_temporal = ImGui.Checkbox("Temporal", self.use_temporal)
 	end,
 
 	postprocess = function(self, env, hdr_buffer, gbuffer, shadowmap) : ()
-		if not self.enabled then return end
+		if not self.enabled then 
+			self.history_buf = -1
+			return 
+		end
+		if self.scale_changed then
+			env.ssao_rb_desc = nil
+			env.ssao_blur_rb_desc = nil
+			self.history_buf = -1 
+		end 
+
 		env.ssao_shader = env.ssao_shader or env.preloadShader("pipelines/ssao.shd")
 		env.blur_shader = env.blur_shader or env.preloadShader("pipelines/blur.shd")
 		env.ssao_blit_shader = env.ssao_blit_shader or env.preloadShader("pipelines/ssao_blit.shd")
 		env.ssao_resolve_shader = env.ssao_resolve_shader or env.preloadShader("pipelines/ssao_resolve.shd")
-		env.ssao_rb_desc = env.ssao_rb_desc or env.createRenderbufferDesc { rel_size = {0.5, 0.5}, format = "r8", debug_name = "ssao", compute_write = true }
+		env.ssao_rb_desc = env.ssao_rb_desc or env.createRenderbufferDesc { rel_size = {self.scale, self.scale}, format = "r8", debug_name = "ssao", compute_write = true }
+		env.ssao_blur_rb_desc = env.ssao_blur_rb_desc or env.createRenderbufferDesc { format = "r8", rel_size = {self.scale, self.scale}, debug_name = "ssao_blur" }
 
-		local w = math.floor(env.viewport_w * 0.5)
-		local h = math.floor(env.viewport_h * 0.5)
+		local w = math.floor(env.viewport_w * self.scale)
+		local h = math.floor(env.viewport_h * self.scale)
 		local ssao_rb = env.createRenderbuffer(env.ssao_rb_desc)
 
 		if self.use_temporal and self.history_buf == -1 then
@@ -40,10 +55,9 @@ return {
 		env.bindImageTexture(ssao_rb, 2)
 		env.dispatch(env.ssao_shader, (w + 15) / 16, (h + 15) / 16, 1)
 
-		env.ssao_blur_rb_desc = env.ssao_blur_rb_desc or env.createRenderbufferDesc { format = "r8", rel_size = {0.5, 0.5}, debug_name = "ssao_blur" }
 		if self.use_temporal then
 			env.beginBlock("ssao_resolve " .. tostring(w) .. "x" .. tostring(h))
-			env.drawcallUniforms( w, h, 0, 0, self.current_frame_weight, 0, 0, 0 )
+			env.drawcallUniforms( w, h, 0, 0, self.current_frame_weight, self.discocclusion_strength, 0, 0 )
 			env.bindTextures({gbuffer.DS, self.history_buf}, 0)
 			env.bindImageTexture(ssao_rb, 2)
 			env.dispatch(env.ssao_resolve_shader, (w + 15) / 16, (h + 15) / 16, 1)
