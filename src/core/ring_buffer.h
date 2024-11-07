dc39fcac90dd6053f2949884a3dc9b26587f9e9b
Mikulas Florek <mikulas.florek@gamedev.sk>
2024-11-07 21:35:10 +0100

    job system optimization

--- src/core/ring_buffer.h
+++ src/core/ring_buffer.h
@@ -5,8 +5,6 @@
 
 namespace Lumix {
 
-#if 1
-
 template <typename T, u32 CAPACITY>
 struct RingBuffer {
 	struct Item {
@@ -82,53 +80,5 @@ struct RingBuffer {
 	Lumix::Mutex mutex;
 };
 
-#else 
-
-// Thread-safe, mutex-guarded ring buffer, with a fallback array for overflow
-template <typename T, u32 CAPACITY>
-struct RingBuffer {
-	static_assert(__is_trivially_copyable(T));
-	RingBuffer(IAllocator& allocator)
-		: m_fallback(allocator)
-	{
-		static_assert(CAPACITY > 2);
-	}
-
-	LUMIX_FORCE_INLINE bool pop(T& obj) {
-		MutexGuardProfiled lock(mutex);
-		const bool is_ring_buffer_empty = wr == rd;
-		if (is_ring_buffer_empty) {
-			if (m_fallback.empty()) return false;
-			
-			obj = m_fallback.back();
-			m_fallback.pop();
-			return true;
-		}
-
-		obj = objects[rd % CAPACITY];
-		++rd;
-		return true;
-	}
-
-	LUMIX_FORCE_INLINE void push(const T& obj) {
-		MutexGuardProfiled lock(this->mutex);
-		if (wr - rd >= CAPACITY) {
-			// buffer full
-			m_fallback.push(obj);
-			return;
-		}
-		
-		objects[wr % CAPACITY] = obj;
-		++wr;
-	}
-
-	Lumix::Mutex mutex;
-	T objects[CAPACITY];
-	u32 rd = 0;
-	u32 wr = 0;
-	Array<T> m_fallback;
-};
-
-#endif
 
 } // namespace Lumix
