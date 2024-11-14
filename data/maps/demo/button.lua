f9d98b6a03249aba9602ff9fe45ef52794bd4e90
Mikulas Florek <mikulas.florek@gamedev.sk>
2024-11-15 00:55:20 +0100

    cleanup

--- data/maps/demo/button.lua
+++ data/maps/demo/button.lua
@@ -9,9 +9,6 @@ cubeB = {}
 sphereA = {}
 sphereB = {}
 sound = -1
-local ik_target_input = -1
-local ik_alpha_input = -1
-local ik_co = nil
 local cubeA_pos = {}
 local cubeB_pos = {}
 local sphereA_pos = {}
@@ -26,6 +23,7 @@ Editor.setPropertyType(this, "sphereB", Editor.ENTITY_PROPERTY)
 Editor.setPropertyType(this, "sound", Editor.RESOURCE_PROPERTY, "clip")
 
 function start()
+	-- remember initial positions so we can reset them when button is pressed
 	cubeA_pos = cubeA.position
 	cubeB_pos = cubeB.position
 	sphereA_pos = sphereA.position
@@ -43,24 +41,6 @@ function update(time_delta)
 	interactive = dist_squared < 2
 	-- animate the label if player is close
 	label.property_animator.enabled = interactive
-
-	if ik_co then
-		local status, res = coroutine.resume(ik_co, time_delta)
-		if not status then
-			LumixAPI.logError("coroutine error: " .. res)
-			ik_co = nil
-		end
-		if not res then
-			ik_co = nil
-		end
-	end
-end
-
-function easeOutBack(x: number): number
-	local c1 = 1.70158;
-	local c3 = c1 + 1;
-	
-	return 1 + c3 * math.pow(x - 1, 3) + c1 * math.pow(x - 1, 2);
 end
 
 function calcIKTarget()
@@ -74,41 +54,43 @@ function calcIKTarget()
 end
 
 function onInputEvent(event : InputEvent)
-	-- check if player pressed "F" and is close
-	if interactive and event.type == "button" and event.device.type == "keyboard" then
-		if event.key_id == string.byte("F") then
-			if event.down then
-				ik_target_input = player.animator:getInputIndex("left_hand_ik_target")
-				ik_alpha_input = player.animator:getInputIndex("left_hand_ik_alpha")
-				
-				player.animator:setVec3Input(ik_target_input, calcIKTarget())
-				co.run(function()
-					co.parallel(
-						-- move hand to the button
-						function() co.lerpAnimatorFloat(player, ik_alpha_input, 0, 1, 0.3) end,
-						-- wait a bit and then press the button
-						function() 
-							co.wait(0.2)
-							co.lerpVec3(this, "local_position", {0, 0, 0}, {0, 0, 0.1}, 0.1)
-						end
-					)
-					-- reset objects' positions
-					cubeA.position = cubeA_pos
-					cubeB.position = cubeB_pos
-					sphereA.position = sphereA_pos
-					sphereB.position = sphereB_pos
-					-- play a sound and wait a bit
-					playSound(sound)
-					co.wait(0.1)
-					co.parallel(	
-						-- move hand back
-						function() co.lerpAnimatorFloat(player, ik_alpha_input, 1, 0, 0.3) end,
-						-- release the button
-						function() co.lerpVec3(this, "local_position", {0, 0, 0.1}, {0, 0, 0}, 0.1) end
-					)
-					return false
-				end)
-			end
-		end
+	-- check if player pressed "F" and the button is interactive
+	if not interactive or event.type ~= "button" or event.device.type ~= "keyboard" then
+		return
 	end
+	
+	if event.key_id ~= string.byte("F") or not event.down then
+		return
+	end
+	
+	local ik_target_input = player.animator:getInputIndex("left_hand_ik_target")
+	local ik_alpha_input = player.animator:getInputIndex("left_hand_ik_alpha")
+	
+	player.animator:setVec3Input(ik_target_input, calcIKTarget())
+	co.run(function()
+		co.parallel(
+			-- move hand to the button
+			function() co.lerpAnimatorFloat(player, ik_alpha_input, 0, 1, 0.3) end,
+			-- wait a bit and then press the button
+			function() 
+				co.wait(0.2)
+				co.lerpVec3(this, "local_position", {0, 0, 0}, {0, 0, 0.1}, 0.1)
+			end
+		)
+		-- reset objects' positions
+		cubeA.position = cubeA_pos
+		cubeB.position = cubeB_pos
+		sphereA.position = sphereA_pos
+		sphereB.position = sphereB_pos
+		-- play a sound and wait a bit
+		playSound(sound)
+		co.wait(0.1)
+		co.parallel(	
+			-- move hand back
+			function() co.lerpAnimatorFloat(player, ik_alpha_input, 1, 0, 0.3) end,
+			-- release the button
+			function() co.lerpVec3(this, "local_position", {0, 0, 0.1}, {0, 0, 0}, 0.1) end
+		)
+		return false
+	end)
 end
