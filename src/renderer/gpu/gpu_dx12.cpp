667b8af6e683808bc07a0b29f8a446c60f469c3a
Mikulas Florek <mikulas.florek@gamedev.sk>
2025-07-08 21:53:13 +0200

    fixed incorrect constant buffer binding - best seen in thumbnails in asset browser

--- src/renderer/gpu/gpu_dx12.cpp
+++ src/renderer/gpu/gpu_dx12.cpp
@@ -2987,10 +2987,9 @@ void draw(const Drawcall& draw) {
 		d->current_program = draw.program;
 	}
 
-	d->cmd_list->SetGraphicsRootConstantBufferView(2, draw.uniform_buffer2->gpu_address + draw.uniform_buffer2_offset);
-
 	if (!setPipelineStateGraphics()) return;
-
+	
+	d->cmd_list->SetGraphicsRootConstantBufferView(2, draw.uniform_buffer2->gpu_address + draw.uniform_buffer2_offset);
 	gpu::BufferHandle vb0 = draw.vertex_buffers[0];
 	const u32 vb0_offset = draw.vertex_buffer_offsets[0];
 	gpu::BufferHandle vb1 = draw.vertex_buffers[1];
