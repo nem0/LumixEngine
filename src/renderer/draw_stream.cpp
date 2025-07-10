8c4d0e99531abb9717813b243163f04d444b299f
Mikulas Florek <mikulas.florek@gamedev.sk>
2025-07-10 23:11:54 +0200

    nearest sampler

--- src/renderer/draw_stream.cpp
+++ src/renderer/draw_stream.cpp
@@ -435,6 +435,7 @@ void DrawStream::createProgram(gpu::ProgramHandle prog
 		SamplerState LinearSamplerClamp : register(s0);
 		SamplerState LinearSampler : register(s1);
 		SamplerState NearestSamplerClamp : register(s2);
+		SamplerState NearestSampler : register(s3);
 
 		#define sampleCubeBindlessLod(sampler, index, uv, lod) bindless_cubemaps[index].Sample((sampler), (uv), (lod))
 		#define sampleCubeBindless(sampler, index, uv) bindless_cubemaps[index].Sample((sampler), (uv))
--- src/renderer/gpu/gpu_dx12.cpp
+++ src/renderer/gpu/gpu_dx12.cpp
@@ -759,6 +759,7 @@ struct SamplerHeap {
 		alloc(device, 0, SamplerFlags::CLAMP_U | SamplerFlags::CLAMP_V | SamplerFlags::CLAMP_W);
 		alloc(device, 1, SamplerFlags::NONE);
 		alloc(device, 2, SamplerFlags::CLAMP_U | SamplerFlags::CLAMP_V | SamplerFlags::CLAMP_W | SamplerFlags::POINT_FILTER);
+		alloc(device, 3, SamplerFlags::POINT_FILTER);
 
 		return true;
 	}
