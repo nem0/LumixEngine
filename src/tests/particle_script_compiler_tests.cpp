7f7daa7c132213a3fc72f9e97dedf5204b75b814
Mikulas Florek <mikulas.florek@gamedev.sk>
2026-01-23 18:31:03 +0100

    trigger ci

--- src/tests/particle_script_compiler_tests.cpp
+++ src/tests/particle_script_compiler_tests.cpp
@@ -2697,7 +2697,7 @@ bool testInputs() {
 	ASSERT_TRUE(fabsf(runner.getOutput(4) - 4.0f) < 0.001f, "i_velocity.y");
 	ASSERT_TRUE(fabsf(runner.getOutput(5) - 6.0f) < 0.001f, "i_velocity.z");
 	ASSERT_TRUE(fabsf(runner.getOutput(6) - 2.5f) < 0.001f, "i_scale");
-	ASSERT_TRUE(false , "test");
+	ASSERT_TRUE(false, "test");
 	return true;
 }
 
