75db8c714dfac2ffa01fdab8c49d354e886b1ff7
Mikulas Florek <mikulas.florek@gamedev.sk>
2026-09-17 01:37:34 +0200

    linux fixes

--- external/evox/capi.h
+++ external/evox/capi.h
@@ -48,8 +48,13 @@ typedef short i16;
 typedef unsigned short u16;
 typedef int i32;
 typedef unsigned int u32;
-typedef long long i64;
-typedef unsigned long long u64;
+#ifdef _WIN32
+	typedef long long i64;
+	typedef unsigned long long u64;
+#else
+	typedef long i64;
+	typedef unsigned long u64;
+#endif
 typedef float f32;
 typedef double f64;
 typedef u64 uintptr;
